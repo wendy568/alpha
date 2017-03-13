e45a11a48438e14832cf8a849e9841eb1a11427a
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-13 16:01:56 +0000

    123

--- alpha-front/ww_edu/application/controllers/Dashboard.php
+++ alpha-front/ww_edu/application/controllers/Dashboard.php
@@ -16,7 +16,7 @@ class Dashboard extends MY_Controller
 
 		$mt4 = $this->TradingAnalysis->export_mt4_datas();
 		$this->load->library('trading_datas_calculate');
-		$data['data'] = $this->trading_datas_calculate->build($mt4, 3)->get_day()->ratio('order_type');
+		$data['data'] = $this->trading_datas_calculate->build($mt4, 3)->get_day()->percent_ratio('order_type');
 		$response = array('archive' => array('status' => 0 ,'message' =>''));
 	
 		encode_json($response,$data);
