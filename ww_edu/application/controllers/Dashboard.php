94df343d7ccad4b7bea2969f02d997de763498c6
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-13 17:06:12 +0000

    123

--- ww_edu/application/controllers/Dashboard.php
+++ ww_edu/application/controllers/Dashboard.php
@@ -63,4 +63,21 @@ class Dashboard extends MY_Controller
 	
 		encode_json($response,$data);
 	}
+
+	public function long_short_ratio()
+	{
+		header( 'Access-Control-Allow-Origin:*' );
+		
+		$this->load->database();
+		$this->load->helper('json');
+		// $this->load->helper('time_zone');
+		// date('Y-m-d H:i:s', time_zone::build()->sundayOfTheWeekOfEnd()->get_time_zone());die;
+		$this->load->model('TradingAnalysis');
+
+		$mt4 = $this->TradingAnalysis->export_mt4_datas();
+		$this->load->library('trading_datas_calculate');
+		$data['data']['percent_ratio'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('percent_ratio', ['order_type'])->get_property();
+	
+		encode_json($response,$data);
+	}
 }
