208fb4aaefa89bcc5e4b7aab7bbf0498cdc0f0f8
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-14 03:11:35 +0000

    123

--- ww_edu/application/models/TradingAnalysis.php
+++ ww_edu/application/models/TradingAnalysis.php
@@ -9,9 +9,10 @@ class TradingAnalysis extends CI_Model
 
     function export_mt4_datas($finency_proc)
     {
+        if(isset($finency_proc)) $where .= " AND order_symbol={$finency_proc}";
     	$map = "SELECT * 
     			FROM mt4_export_datas
-    			WHERE 1=1";
+    			WHERE 1=1 {$where}";
     	
     	$result = $this->db->query($map)->result_array();
     	return $result;
