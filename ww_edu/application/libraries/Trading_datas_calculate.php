fe31861343190660afa990d75b61fdfb1599b530
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-14 16:28:44 +0000

    123

--- ww_edu/application/libraries/Trading_datas_calculate.php
+++ ww_edu/application/libraries/Trading_datas_calculate.php
@@ -24,7 +24,8 @@ class Trading_datas_calculate {
 	private $this_month;
 	private $this_year;
 	private $week = '';
-	private $time_filter_definition;
+	private $time_filter_definition = 'order_close_time';
+	
 	public function build($import_datas, $month = null, $year = null)
 	{
 		$this->_data = $import_datas;
