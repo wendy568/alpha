ccf0f1677b19b76f2029b28b8497226ec9b32d88
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-14 05:43:28 +0000

    123

--- ww_edu/application/controllers/Trading_Analysis.php
+++ ww_edu/application/controllers/Trading_Analysis.php
@@ -43,7 +43,7 @@ class Trading_Analysis extends MY_Controller
 		$mt4 = $this->TradingAnalysis->export_mt4_datas($finency_proc);
 		$this->load->library('trading_datas_calculate');
 		$data['data']['profit_total'] = $this->trading_datas_calculate->build($mt4, 3)->property('sum', ['profit'])->get_property();
-		$data['data']['profit'] = $this->trading_datas_calculate->build($mt4, 3)->saveCountPositive('profit')->property('get_count', ['profit'])->get_property();
+		$data['data']['profit'] = $this->trading_datas_calculate->build($mt4, 3)->property('get_count', ['profit'])->get_property();
 		$data['data']['loss'] = $this->trading_datas_calculate->build($mt4, 3)->get_day()->count()->property('get_count', [])->get_property();
 
 		$response = array('archive' => array('status' => 0 ,'message' =>''));
