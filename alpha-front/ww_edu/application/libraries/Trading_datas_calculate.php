29b62cb6ef092c5a584f3ae76437cb2f4cd7e5de
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-14 19:24:05 +0000

    123

--- alpha-front/ww_edu/application/libraries/Trading_datas_calculate.php
+++ alpha-front/ww_edu/application/libraries/Trading_datas_calculate.php
@@ -72,7 +72,7 @@ class Trading_datas_calculate {
 					$year = date('Y', $v);
 					$month = date('m', $v);
 					$day = date('d', $v);
-					echo 12313;
+					echo $day;
 					if ($year != $this->this_year && $month != $this->this_month && $day != $this->this_day) {
 						unset($datas[$key]);
 					}
