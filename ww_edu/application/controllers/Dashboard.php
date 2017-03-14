6aa161b5bbbba6fde7ec403065d1d12e45de4d5d
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-14 17:25:31 +0000

    123

--- ww_edu/application/controllers/Dashboard.php
+++ ww_edu/application/controllers/Dashboard.php
@@ -16,7 +16,7 @@ class Dashboard extends MY_Controller
 
 		$mt4 = $this->TradingAnalysis->export_mt4_datas();
 		$this->load->library('trading_datas_calculate');
-		print_r($this->trading_datas_calculate->build($mt4, 3)->profit_one_by_one($this->trading_datas_calculate->week));die;
+		print_r($this->trading_datas_calculate->build($mt4, 3)->get_week()->profit_one_by_one($this->trading_datas_calculate->week));die;
 		$response = array('archive' => array('status' => 0 ,'message' =>''));
 	
 		encode_json($response,$data);
