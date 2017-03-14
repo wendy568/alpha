0d33db12cfdb716ed5abb0812c4de7ec9c05b5fe
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-14 02:34:29 +0000

    123

--- ww_edu/application/controllers/Trading_Analysis.php
+++ ww_edu/application/controllers/Trading_Analysis.php
@@ -1,19 +1,23 @@
 <?php
 class Trading_Analysis extends MY_Controller
 {
-	public function trading_previews()
+	public function calculator_anytime()
 	{
 		header( 'Access-Control-Allow-Origin:*' );
-	
-		$xxxx = $this->input->get_post('xxxx', TRUE);
-		$yyyy = $this->input->get_post('yyyy', TRUE);
 		
 		$this->load->database();
 		$this->load->helper('json');
-		$this->load->model('tradinganalysis');
-	
-		$response = array('archive' => array('status' => 0,'message' =>''));
-		$data['data'] = $this->tradinganalysis->trading_previews($xxxx, $yyyy);
+		// $this->load->helper('time_zone');
+		// date('Y-m-d H:i:s', time_zone::build()->sundayOfTheWeekOfEnd()->get_time_zone());die;
+		$this->load->model('TradingAnalysis');
+
+		$mt4 = $this->TradingAnalysis->export_mt4_datas();
+		$this->load->library('trading_datas_calculate');
+		$data['data']['risk_management_level'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('variance', ['profit'])->get_property();
+		$data['data']['operating_frequecy'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('frequency', ['avg_deviation', ['order_open_time', 'order_close_time']])->get_property();
+		$data['data']['operating_accuracy'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('accuracy', ['profit'])->get_property();
+		$data['data']['trading_ability'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('ability', ['profit'])->get_property();
+		$response = array('archive' => array('status' => 0 ,'message' =>''));
 	
 		encode_json($response,$data);
 	}
--- ww_edu/application/core/MY_Controller.php
+++ ww_edu/application/core/MY_Controller.php
@@ -18,6 +18,15 @@ class MY_Controller extends CI_Controller
 		return $data;
 	}
 
+	public function get_trading_account($token)
+	{
+		header( 'Access-Control-Allow-Origin:*' );
+		$this->load->database();
+		$this->load->model('login');
+		$data = $this->login->get_trading_account($token);
+		return $data;
+	}
+
 	public function get_bytoken_id_forAdmin($token)
 	{
 		header( 'Access-Control-Allow-Origin:*' );
