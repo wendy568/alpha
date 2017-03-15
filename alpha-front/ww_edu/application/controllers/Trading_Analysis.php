07bdf2d1ea7efe6e22662f48b48a637ab4f2b71e
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-15 16:04:05 +0000

    123

--- alpha-front/ww_edu/application/controllers/Trading_Analysis.php
+++ alpha-front/ww_edu/application/controllers/Trading_Analysis.php
@@ -17,7 +17,7 @@ class Trading_Analysis extends MY_Controller
 		// date('Y-m-d H:i:s', time_zone::build()->sundayOfTheWeekOfEnd()->get_time_zone());die;
 		$this->load->model('TradingAnalysis');
 
-		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start, $end);
+		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start_time, $end_time);
 		$this->load->library('trading_datas_calculate');
 		$data['data']['risk_management_level'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('variance', ['profit'])->get_property();
 		$data['data']['operating_frequecy'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('frequency', ['avg_deviation', ['order_open_time', 'order_close_time']])->get_property();
@@ -44,7 +44,7 @@ class Trading_Analysis extends MY_Controller
 		// date('Y-m-d H:i:s', time_zone::build()->sundayOfTheWeekOfEnd()->get_time_zone());die;
 		$this->load->model('TradingAnalysis');
 
-		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start, $end);
+		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start_time, $end_time);
 		$this->load->library('trading_datas_calculate');
 		$data['data']['profit_total'] = $this->trading_datas_calculate->build($mt4, 3)->property('sum', ['profit'])->get_property();
 		$data['data']['profit'] = $this->trading_datas_calculate->build($mt4, 3)->saveCountPositive('profit')->property('sum', ['profit'])->get_property();
@@ -71,7 +71,7 @@ class Trading_Analysis extends MY_Controller
 		// date('Y-m-d H:i:s', time_zone::build()->sundayOfTheWeekOfEnd()->get_time_zone());die;
 		$this->load->model('TradingAnalysis');
 
-		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start, $end);
+		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start_time, $end_time);
 		$this->load->library('trading_datas_calculate');
 
 		$data['data']['profit_week'] = $this->trading_datas_calculate->build($mt4, 3)->get_week()->property('get_one_by_one', ['sum', ['profit']])->get_property();
@@ -96,7 +96,7 @@ class Trading_Analysis extends MY_Controller
 		// date('Y-m-d H:i:s', time_zone::build()->sundayOfTheWeekOfEnd()->get_time_zone());die;
 		$this->load->model('TradingAnalysis');
 
-		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start, $end);
+		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start_time, $end_time);
 		$this->load->library('trading_datas_calculate');
 
 		$data['data']['numbers_ratio'] = $this->trading_datas_calculate->build($mt4, 3)->get_week()->property('get_one_by_one', ['ratio', ['order_type']])->get_property();
@@ -121,7 +121,7 @@ class Trading_Analysis extends MY_Controller
 		// date('Y-m-d H:i:s', time_zone::build()->sundayOfTheWeekOfEnd()->get_time_zone());die;
 		$this->load->model('TradingAnalysis');
 
-		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start, $end);
+		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start_time, $end_time);
 		$this->load->library('trading_datas_calculate');
 		$data['data']['percent_ratio'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('percent_ratio', ['order_type'])->get_property();
 		$response = array('archive' => array('status' => 0 ,'message' =>''));
