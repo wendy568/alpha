2ec1ca423567d7ef5e47581d5dea53f0d97569c5
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-15 11:04:29 +0000

    123

--- ww_edu/application/models/TradingAnalysis.php
+++ ww_edu/application/models/TradingAnalysis.php
@@ -52,7 +52,9 @@ class TradingAnalysis extends CI_Model
     {
         $where = "1=1";
         $now = time();
-        var_dump((isset($start_time)));
+        $a = null;
+        $b = ($a) ? $a : false;
+        var_dump((isset($b)));
         if((isset($start_time) && $start_time) OR (isset($end_time) && $end_time)) {
             $start_time = ($start_time) ? $start_time : 0;
             $end_time = ($end_time) ? $end_time : $now;
