7813f6a7e008c27916f8d312a8eb652779254956
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-15 11:13:31 +0000

    123

--- alpha-front/ww_edu/application/models/TradingAnalysis.php
+++ alpha-front/ww_edu/application/models/TradingAnalysis.php
@@ -52,9 +52,7 @@ class TradingAnalysis extends CI_Model
     {
         $where = "1=1";
         $now = time();
-        $a='';
-        $b = ($a) ? $a : false;
-        var_dump(empty($a));
+
         if((isset($start_time) && $start_time) OR (isset($end_time) && $end_time)) {
             $start_time = ($start_time) ? $start_time : 0;
             $end_time = ($end_time) ? $end_time : $now;
