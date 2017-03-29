16503b9b3307776fac1128ee82b50d4559f989b5
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-29 08:06:18 +0000

    123

--- dist/ww_edu/application/controllers/Personal.php
+++ dist/ww_edu/application/controllers/Personal.php
@@ -428,9 +428,8 @@ class Personal extends MY_Controller
 
 	public function test()
 	{
-		print_r(getcwd());die;
 		$row = 1;
-		if (($handle = fopen("2102085730.csv", "r")) !== FALSE) {
+		if (($handle = fopen(getcwd()."/2102085730.csv", "r")) !== FALSE) {
 		    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 		        $num = count($data);
 		        echo "<p> $num fields in line $row: <br /></p>\n";
