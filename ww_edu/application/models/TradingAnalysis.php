cea56042217539f946ef8551a2f7544928ca2a87
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-30 16:24:56 +0000

    123

--- ww_edu/application/models/TradingAnalysis.php
+++ ww_edu/application/models/TradingAnalysis.php
@@ -9,7 +9,6 @@ class TradingAnalysis extends CI_Model
 
     function export_mt4_datas($account = null, $finency_proc = null, $start_time = null, $end_time = null)
     {
-        $where = "1=1";
         $now = time();
         if(isset($finency_proc)) $where .= " AND order_symbol='{$finency_proc}'";
         if(isset($start_time) OR isset($end_time)) {
@@ -19,7 +18,8 @@ class TradingAnalysis extends CI_Model
         }
     	$map = "SELECT * 
     			FROM mt4_export_datas
-    			WHERE {$where}";
+    			WHERE account='{$account}' 
+                AND {$where}";
 
     	$result = $this->db->query($map)->result_array();
     	return $result;
