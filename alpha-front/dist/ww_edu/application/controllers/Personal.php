07a7a65fbe21a4c99e482566d66152c33a9ed4c1
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-03-30 03:40:23 +0000

    123

--- alpha-front/dist/ww_edu/application/controllers/Personal.php
+++ alpha-front/dist/ww_edu/application/controllers/Personal.php
@@ -432,11 +432,10 @@ class Personal extends MY_Controller
 		if (($handle = fopen(getcwd()."/2102085730.csv", "r")) !== FALSE) {
 		    while (($data = fgetcsv($handle, 1000, "|")) !== FALSE) {
 		        $num = count($data);
-		        echo "<p> $num fields in line $row: <br /></p>\n";
-		        $row++;
-		        for ($c=0; $c < $num; $c++) {
-		            echo $data[$c] . "<br />\n";
-		        }
+		        print_r($data);
+		        // for ($c=0; $c < $num; $c++) {
+		        //     echo $data[$c] . "<br />\n";
+		        // }
 		    }
 		    fclose($handle);
 		}
