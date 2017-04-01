6766e8cbbf7043e4a91e87c60230bcbf292d548f
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-01 06:23:16 +0000

    123

--- dist/ww_edu/application/controllers/Personal.php
+++ dist/ww_edu/application/controllers/Personal.php
@@ -445,7 +445,7 @@ class Personal extends MY_Controller
 		if (($handle = fopen(getcwd()."/2102077628.csv", "r")) !== FALSE) {
 		    while (($data = fgetcsv($handle, 0, "|")) !== FALSE) {
 
-		    		$datas[] = @array_combine($mt4_format, $data);
+		    		// $datas[] = array_combine($mt4_format, $data);
 
 		    }
 		    fclose($handle);
