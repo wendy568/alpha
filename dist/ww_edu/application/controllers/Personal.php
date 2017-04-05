4cc02cf9837adcfc790dea725567f49a7f27060f
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-05 03:43:22 +0000

    123

--- dist/ww_edu/application/controllers/Personal.php
+++ dist/ww_edu/application/controllers/Personal.php
@@ -452,7 +452,7 @@ class Personal extends MY_Controller
 		    	// $datas[] = array_combine($mt4_format, $data);
 		    	$i++;
 		    }
-		    fclose($handle);
+		    fclose($handle);1
 		}
 
 		$this->load->helper('databases_filter');
