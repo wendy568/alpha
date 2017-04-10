f6a826380d91f0e3f9c4e49593536f05389a350d
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-10 09:01:47 +0000

    123

--- dist/ww_edu/application/controllers/User.php
+++ dist/ww_edu/application/controllers/User.php
@@ -4,7 +4,7 @@ class User extends MY_Controller
 	public function Login()
 	{
 		header( 'Access-Control-Allow-Origin:*' );
-		setcookie("name","value",time()+$int);
+		setcookie("name","value",time());
 		$data = array();
 		// $username = $this->input->get_post('username', TRUE);
 		$email = $this->input->get_post('email', TRUE);
