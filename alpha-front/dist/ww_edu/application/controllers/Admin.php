c543d20bc4a9c1cf918fc02707b7df8e71d6bcab
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-13 07:54:34 +0000

    123

--- alpha-front/dist/ww_edu/application/controllers/Admin.php
+++ alpha-front/dist/ww_edu/application/controllers/Admin.php
@@ -102,7 +102,7 @@ class Admin extends MY_Controller
 		$this->load->database();
 		$this->load->model('admins');
 		$response = array('archive' => array('status' => 0,'message' =>''));
-		$data['data'] = $this->admins->add($cols, $response);
+		// $data['data'] = $this->admins->add($cols, $response);
 
 		encode_json($response, $data, 201);
 	}
