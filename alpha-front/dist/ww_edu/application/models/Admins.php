8af348eb631377a07d4b9648929a667042925f6e
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-13 07:57:55 +0000

    123

--- alpha-front/dist/ww_edu/application/models/Admins.php
+++ alpha-front/dist/ww_edu/application/models/Admins.php
@@ -129,7 +129,6 @@ class Admins extends CI_Model
 				$status = 39;
 			}
 		});
-		var_dump($response['archive']['message']);
 		$response = array('archive' => array('status' => $status,'message' =>substr($message, 0, -1)));
 	}
 
