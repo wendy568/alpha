6e3841443c2cfc22abb9b18eb32a08b080238edc
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-17 08:35:32 +0000

    123

--- dist/ww_edu/application/libraries/Trading_datas_calculate.php
+++ dist/ww_edu/application/libraries/Trading_datas_calculate.php
@@ -54,11 +54,11 @@ class Trading_datas_calculate{
 
     public function __set($name,$value){
         if(isset($this->import_datas[$name])) {
+        	var_dump($name);
             $this->import_datas[$name] = $value;
         }
 
         if(isset($this->analysis[$name])) {
-        	var_dump($name);
             $this->analysis[$name] = $value;
         }
     }
