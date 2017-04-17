8c94c07987432df7321ba155a27d2b13491dd271
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-17 05:42:23 +0000

    123

--- alpha-front/dist/ww_edu/application/libraries/TradingDatasCalculate.php
+++ alpha-front/dist/ww_edu/application/libraries/TradingDatasCalculate.php
@@ -1,5 +1,18 @@
 <?php  
-
+/**
+	order_no: 订单号
+	account_number: 账户号
+	order_symbol: 货币对
+	order_type: 买卖方向
+	order_lots: 交易手数
+	order_open_price: 开仓价格
+	order_open_time: 开仓时间
+	order_close_price: 平仓价格
+	order_close_time: 平仓时间
+	order_take_profit: 止盈
+	order_stop_loss: 止损
+	profit:盈利
+ */
 class TradingDatasCalculate
 {
 	private $calculate_datas;
