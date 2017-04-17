ad1495c1f48bf1b2b4d7f3efe973e7e51fd8c24a
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-17 07:14:49 +0000

    123

--- dist/ww_edu/application/libraries/Trading_datas_calculate.php
+++ dist/ww_edu/application/libraries/Trading_datas_calculate.php
@@ -1,5 +1,8 @@
 <?php  
 /**
+ 
+ 	MT4 Order Datas
+
 	order_no: 订单号
 	account_number: 账户号
 	order_symbol: 货币对
@@ -13,20 +16,50 @@
 	order_stop_loss: 止损
 	profit:盈利
  */
-class Trading_datas_calculate
-{
-	private $calculate_datas;
+class Trading_datas_calculate{
 
-	private $mt4_datas;
+	private $import_datas = null;
 
-	public function __construct($mt4_datas)
-	{
-		$this->mt4_datas = $mt4_datas;
-		$this->calculate_datas = $calculate_datas;
-	}
+	private $mt4_order_datas = array(
+				'BuyNo' => null,
+				'SellNo' => null,
+				'SingleGain' => null,
+				'Singleloss' => null,
+				'Singlelots' => null,
+				'AvgTime' => null,
+				'AvgGain' => null,
+				'Avgloss' => null,
+				'Period' => null,
+				'GLRatio' => null,
+				'LSRatio' => null,
+				'ProfitLots' => null,
+				'BestSymbol' => null,
+				'RiskManage' => null,
+		);
 
-	public function BuyNo()
-	{
-		return $this;
-	}
+	public function __get($name){
+        if(isset($this->mt4_order_datas[$name])) {
+            return $this->mt4_order_datas[$name];
+        } elseif(isset($this->import_datas)) {
+        	echo 123;
+        }
+        return null;
+    }
+
+    public function __set($name,$value){
+        if(isset($this->mt4_order_datas[$name])) {
+            $this->mt4_order_datas[$name] = $value;
+        } elseif(isset($this->import_datas)) {
+        	echo 123;
+        }
+    }
+
+    public function __isset($name){
+        return isset($this->mt4_order_datas[$name]);
+    }
+
+    public function BuyNo()
+    {
+    	return $this;
+    }
 }
