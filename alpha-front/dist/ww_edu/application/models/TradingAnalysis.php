6ad85bcb6cce477f682d4f29fc6fa5d15d0c6ed7
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-17 05:29:55 +0000

    123

--- alpha-front/dist/ww_edu/application/models/TradingAnalysis.php
+++ alpha-front/dist/ww_edu/application/models/TradingAnalysis.php
@@ -7,4 +7,14 @@ class TradingAnalysis extends CI_Model
         parent::__construct();
     }
 
+    function export_mt4_datas()
+    {
+    	$map = "SELECT * 
+    			FROM mt4_export_datas
+    			WHERE 1=1";
+    	
+    	$result = $this->db->query($map)->result_array();
+    	return $result;
+    }
+
 }
