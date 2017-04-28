f51693005b20cddb4936a1e182746a5d75a6132d
molan <molan@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-28 10:00:52 +0000

    4-28 18:00

--- alpha-front/common/js/common.js
+++ alpha-front/common/js/common.js
@@ -570,30 +570,3 @@ $(function() {
     $.Webarch.init();
 });
 
-// index-video
-// var btn = document.querySelector('.btn-control');
-// btn.onclick = function(){
-//     if(v4.paused){    //当前处于暂停状态
-//         v4.play();
-//         this.src="img/pause.png";
-//     }else {           //当前处于播放状态
-//         v4.pause();
-//         this.src="img/play.png";
-//     }
-// }
-// //鼠标移入视频容器，显示按钮；移出就隐藏
-// var div = document.querySelector('.container');
-// div.onmouseenter = function(){
-//     btn.style.display = 'block';
-// }
-// div.onmouseleave = function(){
-//   btn.style.display = 'none';
-// }
-// //视频暂停则显示广告；播放则隐藏广告
-// var ad = document.querySelector('.ad');
-// v4.onplay = function(){
-//   ad.style.display = 'none';
-// }
-// v4.onpause = function(){
-//   ad.style.display = 'block';
-// }
