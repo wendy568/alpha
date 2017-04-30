c36d5c99092b1f0958a8c1ff42a35641996c63b3
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-04-30 16:26:59 +0000

    123

--- ww_edu/application/controllers/Dashboard.php
+++ ww_edu/application/controllers/Dashboard.php
@@ -118,7 +118,8 @@ class Dashboard extends MY_Controller
 
 		$mt4 = $this->TradingAnalysis->calendar($start_time, $end_time);
 		$this->load->library('trading_datas_calculate');
-		$data['data']['calendar'] = $this->trading_datas_calculate->build($mt4, 3)->count()->get_result();
+		// $data['data']['calendar'] = $this->trading_datas_calculate->build($mt4, 3)->count()->get_result();
+		$data['data']['calendar'] = $this->trading_datas_calculate->build($mt4, 3)->property('getNextOrLastTime', ['+1'，'+6'])->get_property();
 		$response = array('archive' => array('status' => 0 ,'message' =>''));
 		encode_json($response,$data);
 	}
