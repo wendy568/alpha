bbb25261aec01c7a83a4d8d4965634aaa228fe22
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-08 11:13:15 +0000

    123

--- alpha-front/ww_edu/application/controllers/Personal.php
+++ alpha-front/ww_edu/application/controllers/Personal.php
@@ -430,21 +430,6 @@ class Personal extends MY_Controller
 
 	public function test()
 	{
-		class A {
-		    private static $sfoo = 1;
-		    private $ifoo = 2;
-		}
-		$cl1 = static function() {
-		    return A::$sfoo;
-		};
-		$cl2 = function() {
-		    return $this->ifoo;
-		};
-
-		$bcl1 = Closure::bind($cl1, null, 'A');
-		$bcl2 = Closure::bind($cl2, new A(), 'A');
-		echo $bcl1(), "\n";
-		echo $bcl2(), "\n";die;
 		$proxyhost = isset($_POST['proxyhost']) ? $_POST['proxyhost'] : '';
 		$proxyport = isset($_POST['proxyport']) ? $_POST['proxyport'] : '';
 		$proxyusername = isset($_POST['proxyusername']) ? $_POST['proxyusername'] : '';
