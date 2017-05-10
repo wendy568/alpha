4a763a567ecfef7a3e270d7bbcf210d6658695b6
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-10 10:43:48 +0000

    123

--- test_for_php/instance.php
+++ test_for_php/instance.php
@@ -80,35 +80,25 @@ function &get_instance()
 	return instance::get_instance();
 }
 
-class A
-{
-	function __contruct()
-	{
+// class A
+// {
+// 	function hi()
+// 	{
+// 		echo ' Hi ';
+// 	}
+// }
 
-	}
-
-	function hi()
-	{
-		echo ' Hi ';
-	}
-}
-
-class B
-{
-	function __contruct()
-	{
-
-	}
-	
-	function name()
-	{
-		echo ' Chenqi ';
-	}
-}
+// class B
+// {
+// 	function name()
+// 	{
+// 		echo ' Chenqi ';
+// 	}
+// }
 
 $a = &load_class('A');
 $b = &load_class('B');
 
 $test = &get_instance();
-print_r(is_loaded());
+// print_r(is_loaded());
 $test->hi();
