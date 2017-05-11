52a9ee24947994dc5bcee382d5940eb0814017ac
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-11 02:57:16 +0000

    123

--- alpha-front/ww_edu/system/core/CodeIgniter.php
+++ alpha-front/ww_edu/system/core/CodeIgniter.php
@@ -84,7 +84,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 
 if ( ! is_php('5.4'))
-{
+{echo 123;
 	ini_set('magic_quotes_runtime', 0);
 
 	if ((bool) ini_get('register_globals'))
