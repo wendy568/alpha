c7c4b764c2819ee449fd378c3f800fa763baa44f
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-12 12:01:35 +0000

    123

--- test_for_php/Autoload/Register.php
+++ test_for_php/Autoload/Register.php
@@ -7,9 +7,10 @@ class Register
 {
 	public static function Autoload(Common $loader)
 	{
-		array_walk_recursive([
+		$required_classes = [
 			'app' => ['Core\Container' => 'Container'],
-			], function ($val, $key){
+			];
+		array_walk_recursive($required_classes, function ($val, $key){
 				$temp = &$loader::loaded_classes($val, $key);
 			});
 	}
