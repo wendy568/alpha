aa04d44a339109ed2cfaa8e1ef1b5f282caf278a
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-12 12:08:18 +0000

    123

--- test_for_php/Core/Common.php
+++ test_for_php/Core/Common.php
@@ -14,7 +14,7 @@ class Common
 		}
 
 		$name = str_replace('\\', '/', $path) . '/' . $class;
-		print_r(realpath($name . EXT));
+		print_r($name . EXT);
 		if (file_exists(realpath($name . EXT))) {
 			require_once "{$name}" . EXT;
 		}else{
