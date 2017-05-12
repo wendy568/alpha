eef403c4fd3e3ede2b576451a65092dda1f6499e
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-12 18:04:13 +0000

    123

--- ww_edu/application/controllers/Utility.php
+++ ww_edu/application/controllers/Utility.php
@@ -60,6 +60,32 @@ class Utility extends MY_Controller
 	
 		encode_json($response,$data);
 	}
+
+	public function updateTradingLog()
+	{
+
+		header( 'Access-Control-Allow-Origin:*' );
+		
+		$token = $this->input->get_post('token', TRUE);
+		$datas = $this->input->post();
+		$datas['uid'] = $this->get_bytoken($token);
+
+		$this->load->database();
+		$this->load->helper('json');
+		$this->load->helper('sql_operation');
+		$this->load->library('trading_log');
+
+		$cols = $this->trading_log->init($datas)
+								  ->format()
+					              ->property('user_addslashes')
+					              ->update();
+
+		$this->load->model('users');
+		$response = array('archive' => array('status' => 0,'message' =>''));
+		$data['data'] = $this->users->add($cols, $response);
+		
+		encode_json($response,$data);
+	}
 }
 
 
