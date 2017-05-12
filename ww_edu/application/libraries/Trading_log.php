f7785250ad98c8baba35b43dd9e795d867af4368
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-12 17:33:42 +0000

    123

--- ww_edu/application/libraries/Trading_log.php
+++ ww_edu/application/libraries/Trading_log.php
@@ -2,5 +2,24 @@
 
 class Trading_log extends sql_operation
 {
+	public function init()
+	{
+		$this->_data['table'] = 'trading_log';
+		return $this;
+	}
 
+	public function create_time()
+	{
+		$this->_data['create_time'] = date('Y-m-d H:i:s', time());
+		$this->_data['c_time'] = time();
+
+		return $this;
+	}
+
+	public function update_time()
+	{
+		$this->_data['u_time'] = time();
+
+		return $this;
+	}
 }
