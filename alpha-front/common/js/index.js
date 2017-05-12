5890122e0d795ad8967c0284443dd5ce35e4e54a
huaxin <huaxin@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-12 11:51:02 +0000

    pie数据交互

--- alpha-front/common/js/index.js
+++ alpha-front/common/js/index.js
@@ -33,12 +33,83 @@
     });
 
     // synthesizing data 瓶状图----------------------------------------------------------------------------
-    function getPieData(bill) {
+    var billCount = ['AUD/USD','EUR/USD','GBP/USD','NZD/USD','USD/CAD','USD/CHF','USD/CNH','USD/JPY',
+      'AUD/CAD','AUD/CHF','AUD/JPY','AUD/NZD','CAD/CHF','CAD/JPY','CHF/JPY','EUR/AUD','EUR/CAD','EUR/CHF',
+      'EUR/GBP','EUR/JPY','EUR/NZD','GBP/AUD','GBP/CAD','GBP/CHF','GBP/JPY','GBP/NZD','NZD/JPY','XAU/USD',
+      'XAG/USD','DXY','COP/PER','NG/AS','UK/OIL','US/OIL','AUS200','HKG50','HKH40','JPN225','NAS100',
+      'SPX500','UK100','US30'];
 
-    }
-    $.alpha.request_Url('post','http://120.25.211.159/ww_edu/dashboard/long_short_ratio',{},function(data){
-      if(data.archive.status == 0){
-
-      }
+    var pieChart = echarts.init(document.getElementById('ram-usage'));
+    pieChart.setOption({
+      tooltip: {
+        trigger: 'item',
+        formatter: "{b}: {d}%"
+      },
+      series: [
+        {
+          name:'bill',
+          type:'pie',
+          radius: ['50%', '70%'],
+          avoidLabelOverlap: false,
+          label: {
+            normal: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              show: true,
+              textStyle: {
+                fontSize: '16',
+                fontWeight: 'bold'
+              }
+            }
+          },
+          labelLine: {
+            normal: {
+              show: false
+            }
+          },
+          data:[]
+        }
+      ]
     });
+
+
+    function getPieData(bill) {
+      bill = bill.replace('/','');
+      $.alpha.request_Url('post','dashboard/long_short_ratio',{finency_proc:bill},function(data){
+        if(data.archive.status == 0){
+          var billData = [];
+          billData[0] = {name:'BUY',value:data.data.percent_ratio._0};
+          billData[1] = {name:'SELL',value:data.data.percent_ratio._1};
+          pieChart.setOption({
+            series:[{
+              name: 'bill',
+              data: billData
+            }]
+          });
+        }
+      });
+    }
+
+    getPieData($('.yun span').text());
+
+    $('.yun a').eq(0).click(function (e) {
+      e.stopPropagation();
+      var curBillType = $('.yun span').text();
+      var curIndex = billCount.indexOf(curBillType);
+      var preIndex = curIndex <= 0 ? 0 : curIndex - 1;
+      getPieData(billCount[preIndex]);
+      $('.yun span').text(billCount[preIndex]);
+    });
+
+    $('.yun a').eq(1).click(function (e) {
+      e.stopPropagation();
+      var curBillType = $('.yun span').text();
+      var curIndex = billCount.indexOf(curBillType);
+      var preIndex = curIndex >= billCount.length - 1 ? (billCount.length - 1) : curIndex + 1;
+      getPieData(billCount[preIndex]);
+      $('.yun span').text(billCount[preIndex]);
+    });
+
 })();
