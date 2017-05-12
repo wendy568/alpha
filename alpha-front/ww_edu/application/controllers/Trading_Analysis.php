41b2bb497e8dbee1acbb31783d02815f667cdb7f
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-12 06:39:39 +0000

    123

--- alpha-front/ww_edu/application/controllers/Trading_Analysis.php
+++ alpha-front/ww_edu/application/controllers/Trading_Analysis.php
@@ -124,9 +124,42 @@ class Trading_Analysis extends MY_Controller
 		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start_time, $end_time);
 		$this->load->library('trading_datas_calculate');
 		$data['data']['percent_ratio'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('percent_ratio', ['order_type'])->get_property();
-		$response = array('archive' => array('status' => 0 ,'message' =>''));
 		
+		$response = array('archive' => array('status' => 0 ,'message' =>''));
+
 		encode_json($response,$data);
 	}
+
+	public function allTradingStatistics()
+	{
+		header( 'Access-Control-Allow-Origin:*' );
+		
+		$token = $this->input->get_post('token', TRUE);
+		$start_time = $this->input->get_post('start_time', TRUE);
+		$end_time = $this->input->get_post('end_time', TRUE);
+		$finency_proc = $this->input->get_post('finency_proc', TRUE);
+		$account = $this->get_trading_account($token);
+
+		$this->load->database();
+		$this->load->helper('json');
+		// $this->load->helper('time_zone');
+		// date('Y-m-d H:i:s', time_zone::build()->sundayOfTheWeekOfEnd()->get_time_zone());die;
+		$this->load->model('TradingAnalysis');
+
+		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start_time, $end_time);
+		$this->load->library('trading_datas_calculate');
+		$data['data']['Net_profit'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('sum', ['profit'])->get_property();
+		$data['data']['Average_Profits'] = $this->trading_datas_calculate->build($mt4, 3)->saveCountPositive('profit')->count()->property('avg', ['profit'])->get_property();
+		$data['data']['Average_Loss'] = $this->trading_datas_calculate->build($mt4, 3)->saveCountNegative('profit')->count()->property('avg', ['profit'])->get_property();
+		$data['data']['Maximum_Consecutive_Profit'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('percent_ratio', ['order_type'])->get_property();
+		$data['data']['Avg_holding_Time'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('avg_deviation', ['order_open_time', 'order_close_time'])->get_property();
+		$data['data']['risk_management_level'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('variance', ['profit'])->get_property();
+
+		$response = array('archive' => array('status' => 0 ,'message' =>''));
+
+		encode_json($response,$data);
+	}
+
+
 
 }
