77fbd3aa2c1f05f7393e32fec0eadd099247b95b
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 20:44:54 +0000

    123

--- ww_edu/application/libraries/Classes_mission.php
+++ ww_edu/application/libraries/Classes_mission.php
@@ -37,8 +37,9 @@ class Classes_mission
 
 	private function viewVideo()
 	{
-		$diff1 = [1,2,3,4];
-		$diff2 = [];
+		$diff1 = $this->mission['Video Learning'];
+		$diff2 = $this->homework['Video Learning'];
+		print_r($diff1);print_r($diff2);
 		return array_diff($diff1, $diff2);
 	}
 
