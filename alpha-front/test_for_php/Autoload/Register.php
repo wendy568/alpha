43f1bf2cf9646a79775a2e7bd117c7eb1b76bec2
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 13:48:17 +0000

    123

--- alpha-front/test_for_php/Autoload/Register.php
+++ alpha-front/test_for_php/Autoload/Register.php
@@ -9,7 +9,7 @@ class Register
 		$this->Autoload();
 	}
 
-	public function Autoload()
+	private function Autoload()
 	{
 		$required_classes = [
 			'core'    => [
@@ -20,18 +20,36 @@ class Register
 		    // 			    'russian'       => 'App'
 		    // 			 ],
 		    'kernel'  => [
-		    'WebInterFace'    => 'Kernel',
 		    				'TestInterFace'   => 'Kernel',
 		    				'App'    		  => 'Kernel',
 		    				'AppInterface'    => 'Kernel', 
 		    				'FactoryAbstract' => 'Kernel',
-		    				
+		    				'WebInterFace'    => 'Kernel',
 		    			 ],
 			];
 
 		array_walk_recursive($required_classes, function ($val, $key) {
 				$classes = &\Core\loaded_interfaceAbstracts($key, $val);
 			});
+	}
+
+	private function load_core()
+	{
+
+		$required_classes = [
+		    'kernel'  => [
+		    				'AppInterface'    => 'Kernel',
+		    				'TestInterFace'   => 'Kernel',
+		    				'App'    		  => 'Kernel',
+		    				'FactoryAbstract' => 'Kernel',
+		    				'WebInterFace'    => 'Kernel',
+		    			 ],
+			];
+
+		array_walk_recursive($required_classes, function ($val, $key) {
+				$classes = &\Core\loaded_interfaceAbstracts($key, $val);
+			});
+		
 	}
 
 }
