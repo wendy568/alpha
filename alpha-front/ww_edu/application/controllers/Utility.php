0be31ceed1c322f99d163096e9ba1e9254ea6923
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 12:55:40 +0000

    123

--- alpha-front/ww_edu/application/controllers/Utility.php
+++ alpha-front/ww_edu/application/controllers/Utility.php
@@ -48,7 +48,7 @@ class Utility extends MY_Controller
 		$this->load->helper('sql_operation');
 		$this->load->library('trading_log');
 
-		$cols = $this->trading_log->format($datas)->property('user_addslashes')->add();
+		$cols = $this->trading_log->format($datas)->property('user_addslashes')->update();
 
 		$this->load->model('users');
 		$response = array('archive' => array('status' => 0,'message' =>''));
