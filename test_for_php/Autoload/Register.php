d571d699f9055ba36af2e53913f5061c69ee7bdf
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 08:13:50 +0000

    123

--- test_for_php/Autoload/Register.php
+++ test_for_php/Autoload/Register.php
@@ -9,14 +9,20 @@ class Register
 	public static function Autoload(Common $loader)
 	{
 		$required_classes = [
-			'core'    => ['Core'   => 'Container'],
-			'kernel'  => ['Kernel' => 'AppInterface', 
-						  'Kernel' => 'App',
-						  'Kernel' => 'FactoryAbstract',
-						  'Kernel' => 'WebInterFace',
-						  'Kernel' => 'TestInterFace'],
-		    'App'     => ['App'    => 'activeRussian',
-		    			  'App'    => 'russian'],
+			'core'    => [
+							'Core'   => 'Container'
+						 ],
+			'kernel'  => [
+							'Kernel' => 'AppInterface', 
+							'Kernel' => 'App',
+							// 'Kernel' => 'FactoryAbstract',
+							// 'Kernel' => 'WebInterFace',
+							// 'Kernel' => 'TestInterFace'
+						 ],
+		    'App'     => [
+		    				'App'    => 'activeRussian',
+		    			    'App'    => 'russian'
+		    			 ],
 			];
 
 		array_walk_recursive($required_classes, function ($val, $key) use ($loader) {
