20ff45359f56671ddc3e76b393c5024dcf31db83
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 12:59:17 +0000

    123

--- test_for_php/Core/Common.php
+++ test_for_php/Core/Common.php
@@ -1,68 +1,64 @@
 <?php  
 namespace Core;
 
-class Common
+function &loaded_classes($class, $path, $param = null)
 {
 
-	public static function &loaded_classes($class, $path, $param = null)
+	static $_classes = array();
+
+	if (isset($_classes[$class]))
 	{
-
-		static $_classes = array();
-
-		if (isset($_classes[$class]))
-		{
-			return $_classes[$class];
-		}
-
-		$name = str_replace('\\', '/', $path) . '/' . $class;
-		if (file_exists(realpath($name . EXT))) {
-			require_once "{$name}" . EXT;
-		}else{
-			exit("HAVE NOT SUCH {$name} IN SYSTEM");
-		}
-
-		static::is_loaded($class, $path);
-
-		$instance = '\\' . $path . '\\' . $class;
-
-		$_classes[$class] = isset($param)
-			? new $instance($param)
-			: new $instance();
-		return $_classes[$class];
-		
-	}
-
-	public static function &loaded_interfaceAbstracts($class, $path)
-	{
-
-		static $_classes = array();
-
-		if (isset($_classes[$class]))
-		{
-			return $_classes[$class];
-		}
-
-		$name = str_replace('\\', '/', $path) . '/' . $class;
-		if (file_exists(realpath($name . EXT))) {
-			require_once "{$name}" . EXT;
-		}else{
-			exit("HAVE NOT SUCH {$name} IN SYSTEM");
-		}
-
-		$_classes[$class] = '\\' . $path . '\\' . $class;
 		return $_classes[$class];
 	}
 
-	public static function &is_loaded($class = '', $path = '')
-	{
-		static $_is_loaded = array();
-
-		if ($class !== '')
-		{
-			$_is_loaded['\\' . $path . '\\' . $class] = $class;
-		}
-
-		return $_is_loaded;
-		
+	$name = str_replace('\\', '/', $path) . '/' . $class;
+	if (file_exists(realpath($name . EXT))) {
+		require_once "{$name}" . EXT;
+	}else{
+		exit("HAVE NOT SUCH {$name} IN SYSTEM");
 	}
+
+	static::is_loaded($class, $path);
+
+	$instance = '\\' . $path . '\\' . $class;
+
+	$_classes[$class] = isset($param)
+		? new $instance($param)
+		: new $instance();
+	return $_classes[$class];
+	
+}
+
+function &loaded_interfaceAbstracts($class, $path)
+{
+
+	static $_classes = array();
+
+	if (isset($_classes[$class]))
+	{
+		return $_classes[$class];
+	}
+
+	$name = str_replace('\\', '/', $path) . '/' . $class;
+	if (file_exists(realpath($name . EXT))) {
+		require_once "{$name}" . EXT;
+	}else{
+		exit("HAVE NOT SUCH {$name} IN SYSTEM");
+	}
+
+	$_classes[$class] = '\\' . $path . '\\' . $class;
+	return $_classes[$class];
+}
+
+function &is_loaded($class = '', $path = '')
+{
+	static $_is_loaded = array();
+
+	if ($class !== '')
+	{
+		$_is_loaded['\\' . $path . '\\' . $class] = $class;
+	}
+
+	return $_is_loaded;
+	
 }
--- test_for_php/index.php
+++ test_for_php/index.php
@@ -6,9 +6,12 @@ require_once 'Autoload' . '/' . 'Register' . EXT;
 require_once 'Core' . '/' . 'Common' . EXT;
 
 use Autoload\Register;
-use Core\Common;
 use Core\Container;
 
-(new Register(new Common()))->Autoload();
-print_r(Container::locker());
+(new Register())->Autoload();
+
+function &instance(){
+	return Container::locker();
+}
+print_r($instance);
 // new App(new activeRussian('ChenQi', 27));
