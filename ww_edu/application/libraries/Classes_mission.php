fc0ad3549a375f4a1b67188d1e4ec57e1daf5dda
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 21:27:31 +0000

    123

--- ww_edu/application/libraries/Classes_mission.php
+++ ww_edu/application/libraries/Classes_mission.php
@@ -42,8 +42,9 @@ class Classes_mission
 		array_walk_recursive($this->category, function ($val, $key) use (&$category) {
 			$category[] = $key;
 		});
-		print_r($this->homework);
 		array_walk_recursive($this->homework, function ($val, $key) use (&$homework) {
+			var_dump($val);
+			var_dump($key);
 			$homework[] = $key;
 		});
 		print_r($homework);
