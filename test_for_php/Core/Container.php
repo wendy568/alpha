5ba106fb67e67a75336436bc21b3773bd7d0fdb4
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 06:45:53 +0000

    123

--- test_for_php/Core/Container.php
+++ test_for_php/Core/Container.php
@@ -10,7 +10,8 @@ class Container
 	public function __contruct()
 	{
 		static::$_container = $this;
-		print_r(Common::is_loaded);
+		print_r(Common::is_loaded());
+		print_r(Common::loaded_classes());
 		array_walk(Common::is_loaded(), function ($val, $key) {
 			$this->$val = &Common::loaded_classes($val, $key);
 		});
