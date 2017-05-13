82dab3e79a62b5c8d0f48c11ae179c3f29978683
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 12:49:54 +0000

    123

--- alpha-front/test_for_php/index.php
+++ alpha-front/test_for_php/index.php
@@ -7,6 +7,7 @@ require_once 'Core' . '/' . 'Common' . EXT;
 
 use Autoload\Register;
 use Core\Common;
+use Core\Container;
 
 (new Register(new Common()))->Autoload();
 print_r(Container::is_loaded());
