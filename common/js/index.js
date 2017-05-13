4e4d5cf334ad96c6e97b9e862545d5340e3846a0
huaxin <huaxin@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 05:48:42 +0000

    财经日历

--- common/js/index.js
+++ common/js/index.js
@@ -31,78 +31,80 @@
         $('.chart .label-info').eq(1).html(data.data.trading_ability);
       }
     });
+
     //折线图
     $.alpha.request_Url('post','dashboard/profit_statistics',{},function(data){
+      if(data.archive.status == 0){
         var date=data.data.profit_week;
         var key=[];
         var currData=[];
 
         for(var i in date){
-            key.push(i);
-            var cur = 0;
-            cur = !date[i] ? 0 : date[i];
-            currData.push(cur);
+          key.push(i);
+          var cur = 0;
+          cur = !date[i] ? 0 : date[i];
+          currData.push(cur);
         }
 
         var lineChart = echarts.init(document.getElementById('lineChart'),'purple-passion');
         var option = {
-            title: {},
-            tooltip : {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'cross',
-                    label: {
-                        backgroundColor: '#6a7985',
-                    }
-                }
+          title: {},
+          tooltip : {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
+                backgroundColor: '#6a7985',
+              }
+            }
+          },
+          legend: {},
+          toolbox: {
+            feature: {
+              saveAsImage: {}
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              type : 'category',
+              boundaryGap : false,
+              data : key,
+              textStyle : {
+                color: '#fff'
+              },
+            }
+          ],
+          yAxis : [
+            {
+              type : 'value'
+            }
+          ],
+          series : [
+            {
+              name:'profit_week',
+              type:'line',
+              areaStyle: {normal: {}},
+              data:currData
             },
-            legend: {},
-            toolbox: {
-                feature: {
-                    saveAsImage: {}
-                }
-            },
-            grid: {
-                left: '3%',
-                right: '4%',
-                bottom: '3%',
-                containLabel: true
-            },
-            xAxis: [
-                {
-                    type : 'category',
-                    boundaryGap : false,
-                    data : key,
-                    textStyle : {
-                        color: '#fff'
-                    },
-                }
-            ],
-            yAxis : [
-                {
-                    type : 'value'
-                }
-            ],
-            series : [
-                {
-                    name:'profit_week',
-                    type:'line',
-                    areaStyle: {normal: {}},
-                    data:currData
-                },
 
-            ]
+          ]
         };
         // 使用刚指定的配置项和数据显示图表。
         lineChart.setOption(option);
-
+      }
     });
 
-    // synthesizing data 瓶状图----------------------------------------------------------------------------
+    // synthesizing data 环形图----------------------------------------------------------------------------
     var billCount = ['AUD/USD','EUR/USD','GBP/USD','NZD/USD','USD/CAD','USD/CHF','USD/CNH','USD/JPY',
       'AUD/CAD','AUD/CHF','AUD/JPY','AUD/NZD','CAD/CHF','CAD/JPY','CHF/JPY','EUR/AUD','EUR/CAD','EUR/CHF',
-      'EUR/GBP','EUR/JPY','EUR/NZD','GBP/AUD','GBP/CAD','GBP/CHF','GBP/JPY','GBP/NZD','NZD/JPY','XAU/USD',
-      'XAG/USD','DXY','COP/PER','NG/AS','UK/OIL','US/OIL','AUS200','HKG50','HKH40','JPN225','NAS100',
+      'EUR/GBP','EUR/JPY','EUR/NZD','GBP/AUD','GBP/CAD','GBP/CHF','GBP/JPY','GBP/NZD','NZD/JPY','GOLD',
+      'AUG','DXY','COPPER','NGAS','UKOIL','USOIL','AUS200','HKG50','HKH40','JPN225','NAS100',
       'SPX500','UK100','US30'];
 
     var pieChart = echarts.init(document.getElementById('ram-usage'),'purple-passion');
@@ -208,5 +210,40 @@
       getPieData(billCount[preIndex]);
       $('.yun span').text(billCount[preIndex]);
     });
+
+    // synthesizing data 财经日历----------------------------------------------------------------------------
+    var firstDate = 0;
+    var weeks = ['SUN','MON','TUE','WEN','THUR','FRI','SAT'];
+    function getCalendarData(firstDate,direction) {
+      var date = {
+        left_right : direction || '',
+        time_node : firstDate || ''
+      };
+      $.alpha.request_Url('post','dashboard/calendar',date,function(data){
+        if(data.archive.status == 0){
+
+          var dateList = [];
+          var html = '';
+          var isCurDay = false;
+          $.each(data.data.calendar,function (i,item) {
+            dateList.push(i);
+            i = i.replace('.','-').replace('.','-');
+            var curDay = new Date(i);
+            var month = curDay.getMonth()+1 < 10 ? '0' + (curDay.getMonth()+1) : curDay.getMonth()+1;
+            var day = curDay.getDate() < 10 ? '0' + curDay.getDate() : curDay.getDate();
+            isCurDay = new Date().getDay() == curDay.getDay();
+            activeClass = isCurDay ? 'active' : '';
+            html += '<li class="inner-col item date">' +
+                         '<div class="text-c1 small-text text-center ' + activeClass + '">'+weeks[curDay.getDay()]+'</div>' +
+                             '<div class="text-c3 text-center">'+ month +'/' + day + '</div>'+
+                    '</li>';
+          });
+
+          $('.En-clander .clander-tab').html(html);
+        }
+      });
+    }
+
+  getCalendarData();
 
 })();
