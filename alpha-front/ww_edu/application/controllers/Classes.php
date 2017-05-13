3ad80832f842a9c7b91f8e3b4fc0990a89eb0720
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 20:51:51 +0000

    123

--- alpha-front/ww_edu/application/controllers/Classes.php
+++ alpha-front/ww_edu/application/controllers/Classes.php
@@ -19,7 +19,7 @@ class Classes extends MY_Controller
 		$response = array('archive' => array('status' => 0,'message' =>''));
 		$original = $this->ClassesM->current_stage($uid);
 		$data['data']['current_stage'] = $this->classes_mission->jsonDecode($original['personal']['homework']);
-		$data['data']['process'] = $this->classes_mission->init($data['data']['current_stage'], $this->classes_mission->jsonDecode($original['mission']['homework']))->property('viewVideo')->get_property();
+		$data['data']['process'] = $this->classes_mission->init($this->classes_mission->jsonDecode($original['mission']['homework'], $data['data']['current_stage']))->property('viewVideo')->get_property();
 		encode_json($response,$data);
 	}
 
