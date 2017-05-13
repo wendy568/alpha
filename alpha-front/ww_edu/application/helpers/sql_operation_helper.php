59fb01a640f0dd6b65a6b170f7261dac22f2b9dc
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 12:32:57 +0000

    123

--- alpha-front/ww_edu/application/helpers/sql_operation_helper.php
+++ alpha-front/ww_edu/application/helpers/sql_operation_helper.php
@@ -28,6 +28,7 @@ abstract class sql_operation
 		}
 
 		if(isset($this->_data[$index]) && $this->_data[$index]) {
+			echo 123;
 			return addslashes($this->_data[$index]);
 		} else {
 			return false;
