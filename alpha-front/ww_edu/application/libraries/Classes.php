a710bb8b31d03b0273570819363c82c61f4044de
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-13 18:21:04 +0000

    123

---
+++ alpha-front/ww_edu/application/libraries/Classes.php
@@ -0,0 +1,42 @@
+<?php  
+
+class Classes
+{
+	private $category = [
+				['Video Learning'],
+				['Article learning'],
+				['Place your order', 'Make Transactions', 'Make Transaction 1', 'Make Transaction 2'],
+				['4 style trade'],
+				['take profits/stop loss'],
+				['Trade all kinds products', 'Task 2 - 10 different products', '5 tradable products'],
+				['Trading Record'],
+				['Learning Report'],
+				['Trading Score'],
+				['Task 1 - 2 different markets'],
+				['Produce a module'],
+				['Risk Management Level'],
+				['Profitable Period']
+			];
+
+	private $mission = '';
+
+	private $homework = '';
+
+	private $videoId;
+
+	private $articleId;
+	
+	public function init($mission, $homework)
+	{
+		$this->mission = $mission;
+		$this->homework = $homework;
+		return $this;
+	}
+
+	private function viewVideo()
+	{
+
+	}
+
+
+}
