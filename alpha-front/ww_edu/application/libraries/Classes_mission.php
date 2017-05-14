fe906190a3e2d5d38a5bed6d7e8675a6b13d81fa
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-14 16:32:58 +0000

    123

--- alpha-front/ww_edu/application/libraries/Classes_mission.php
+++ alpha-front/ww_edu/application/libraries/Classes_mission.php
@@ -42,8 +42,9 @@ class Classes_mission
 		$this->missionCount = count($mission);
 
 		$result = [];
-		print_r($allProcess);
+
 		foreach($allProcess as $key => $value) {
+			print_r($value);
 			foreach ($this->jsonDecode($value['homework']) as $k => $v) {
 				$v = is_array($v) ? 'is_view' : $v;
 				$v = is_numeric($v) ? 'isCountEnough' : $v;
