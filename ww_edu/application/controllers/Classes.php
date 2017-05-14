02df34590e61c11a1bdc7213545cbd1a2c7f8c50
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-14 02:02:42 +0000

    123

--- ww_edu/application/controllers/Classes.php
+++ ww_edu/application/controllers/Classes.php
@@ -35,8 +35,15 @@ class Classes extends MY_Controller
 	}
 
 	public function showData($ids, $table)
-	{		
-		return $this->classes_mission->showData($ids, $table);
+	{
+		header( 'Access-Control-Allow-Origin:*' );
+		
+		$this->load->database();
+		$this->load->model('ClassesM');
+	
+		$response = array('archive' => array('status' => 0,'message' =>''));
+
+		return $this->ClassesM->showData($ids, $table);
 	}
 
 	public function All_stages()
