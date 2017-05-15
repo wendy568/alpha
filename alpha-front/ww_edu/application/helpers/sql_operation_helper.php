4bf15fe829b54210d4054dc1c7a037eb2c3af62b
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-15 10:36:12 +0000

    123

--- alpha-front/ww_edu/application/helpers/sql_operation_helper.php
+++ alpha-front/ww_edu/application/helpers/sql_operation_helper.php
@@ -35,6 +35,7 @@ abstract class sql_operation
 
 	public function property($callback, $param = [])
 	{
+		echo 123;die;
 		$this->_data = call_user_func_array([$this, $callback], $param);
 		return $this;
 	}
