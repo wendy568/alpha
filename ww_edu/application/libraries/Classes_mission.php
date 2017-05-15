ee4a8c1397fe36cc9820021ab7d63fdd190450e6
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-15 23:27:00 +0000

    123

--- ww_edu/application/libraries/Classes_mission.php
+++ ww_edu/application/libraries/Classes_mission.php
@@ -260,12 +260,12 @@ class Classes_mission
 		call_user_func_array([$this, 'count'], ['trading_count', $param, [$this->account]]);
 	}
 
-	protected function orderSymbolCount($param)
-	{
-		call_user_func_array([$this, 'count'], ['trading_count_group', $param, [$this->account, 'order_type', '0,1,2,3,4,5', 'order_type']]);
-	}
+	// protected function orderSymbolCount($param)
+	// {
+	// 	call_user_func_array([$this, 'count'], ['trading_count_group', $param, [$this->account, 'order_type', '0,1,2,3,4,5', 'order_type']]);
+	// }
 
-	protected function specCountProc($param)
+	protected function orderSymbolCount($param)
 	{
 		call_user_func_array([$this, 'count'], ['tradingCountGL', $param, [$this->account, 'order_take_profit']]);
 		call_user_func_array([$this, 'count'], ['tradingCountGL', $param, [$this->account, 'order_stop_loss']]);
