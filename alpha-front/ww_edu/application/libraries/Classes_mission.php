9071cbd6618b2a27e7fcaac6b82cfd948d30b7f1
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-15 22:55:42 +0000

    123

--- alpha-front/ww_edu/application/libraries/Classes_mission.php
+++ alpha-front/ww_edu/application/libraries/Classes_mission.php
@@ -264,7 +264,7 @@ class Classes_mission
 
 	protected function orderSymbolCount($param)
 	{
-		// call_user_func_array([$this, 'count'], ['trading_count', [$param, [$this->account, 'order_type', '0,1,2,3,4,5', 'order_type']]]);
+		call_user_func_array([$this, 'count'], ['trading_count', $param, [$this->account, 'order_type', '0,1,2,3,4,5', 'order_type']]);
 	}
 
 	protected function specCountProc($param)
