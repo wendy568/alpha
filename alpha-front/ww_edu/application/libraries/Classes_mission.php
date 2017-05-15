63fe435be85767213bb49df50f51b182eb41ae27
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-15 01:41:03 +0000

    123

--- alpha-front/ww_edu/application/libraries/Classes_mission.php
+++ alpha-front/ww_edu/application/libraries/Classes_mission.php
@@ -154,8 +154,8 @@ class Classes_mission
 			if (($is_mission = array_search($this->public_params, $this->mission[$key])) && $is_mission !== false) {
 				var_dump($this->mission[$key][$is_mission]);
 				var_dump($value);
-				var_dump(array_search($this->mission[$key][$is_mission], $value) ? false : $this->mission[$key][$is_mission]);
-				$param[$key][] = array_search($this->mission[$key][$is_mission], $value) ? false : $this->mission[$key][$is_mission];
+				var_dump(array_search($this->mission[$key][$is_mission], $value) ? : $this->mission[$key][$is_mission]);
+				$param[$key][] = array_search($this->mission[$key][$is_mission], $value) ? : $this->mission[$key][$is_mission];
 			}
 		}
 		
