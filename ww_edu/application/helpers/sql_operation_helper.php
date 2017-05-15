b2e0ef9328de137cf27b0afe1fbe1706142a5de5
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-15 11:39:41 +0000

    123

--- ww_edu/application/helpers/sql_operation_helper.php
+++ ww_edu/application/helpers/sql_operation_helper.php
@@ -27,8 +27,9 @@ abstract class sql_operation
 		}
 
 		if(isset($this->_data[$index]) && $this->_data[$index]) {
-			echo 123;
+
 			return addslashes($this->_data[$index]);
+			print_r($this->_data[$index]);
 		} else {
 			return false;
 		}
