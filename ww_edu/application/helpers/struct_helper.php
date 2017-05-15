5d9e30b13b28d376908f2660f968f0c37bed1a23
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-15 11:14:44 +0000

    123

--- ww_edu/application/helpers/struct_helper.php
+++ ww_edu/application/helpers/struct_helper.php
@@ -111,6 +111,7 @@ trait struct
 
 	public function property($callback, $param = [])
 	{
+		$param = null;
 		var_dump(isset($param));die;
 		$this->property = call_user_func_array([$this, $callback], $param);
 		return $this;
