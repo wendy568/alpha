dccb09a31806fade1acc172844c1e455ae9ee735
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-16 00:15:31 +0000

    123

--- ww_edu/application/models/TradingAnalysis.php
+++ ww_edu/application/models/TradingAnalysis.php
@@ -96,7 +96,7 @@ class TradingAnalysis extends CI_Model
                 WHERE account_number='{$account}' {$where} {$group}";
 
         $result = $this->db->query($map)->row_array();
-        print_r(count($result));
+        print_r($result);
         return count($result);
     }
 
