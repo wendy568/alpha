3ca5f07bc711989ecbb7ff347b086633bc565702
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-16 02:17:48 +0000

    123

--- alpha-front/ww_edu/application/models/Trading_logs.php
+++ alpha-front/ww_edu/application/models/Trading_logs.php
@@ -22,4 +22,19 @@ class Trading_logs extends CI_Model
         return $result;
 	}
 
+	function logs_count($uid, $time, $col = null, $param = null)
+	{
+	    $where = null;
+	    if ((isset($col) && $col) && (isset($param) && $param)) $where = " AND {$col} IN ({$param})";
+
+	    $map = "SELECT count(*) AS count  
+	            FROM trading_log
+	            WHERE uid='{$uid}' AND c_time>{$time} {$where}";
+
+	    $result = $this->db->query($map)->row_array();
+
+	    return $result['count'];
+	}
+
+
 }
