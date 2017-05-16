03065f43d78bd0f3395ffd6d3f31d508e28ec148
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-16 03:09:09 +0000

    123

--- ww_edu/application/libraries/Classes_mission.php
+++ ww_edu/application/libraries/Classes_mission.php
@@ -291,6 +291,7 @@ class Classes_mission
 
 	protected function homework_ability($param)
 	{
+		print_r($this->_data);
 		call_user_func_array([$this, 'accuracy'], ['export_mt4_datas', $param, 'TradingAnalysis', [$this->account, null, null, null, $this->time]]);
 		$ability = $this->ability('profit');
 		print_r($ability);
