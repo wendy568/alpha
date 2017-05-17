b3bebf6436427bb2ad408e7de3e1ac9d01cca706
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-17 16:11:12 +0000

    123

--- ww_edu/application/libraries/Classes_mission.php
+++ ww_edu/application/libraries/Classes_mission.php
@@ -334,7 +334,7 @@ class Classes_mission
 				$mon = getdate($this->time)['mon'];
 				$mon = $mon + $i;
 				$profit = $this->build($mt4,$mon)->get_month()->property('sum', ['profit'])->get_property();
-				// print_r($profit);
+				var_dump($profit);
 				$count += ($profit > 0) ? 1 : 0;
 			}
 		}
