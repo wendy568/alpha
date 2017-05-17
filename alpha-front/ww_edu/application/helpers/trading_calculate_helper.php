102dd6b609d94c27caba63cab3d4e796cfb95ba4
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-17 23:00:38 +0000

    123

--- alpha-front/ww_edu/application/helpers/trading_calculate_helper.php
+++ alpha-front/ww_edu/application/helpers/trading_calculate_helper.php
@@ -165,6 +165,7 @@ trait Trading_calculate  {
 
     protected function variance_score($variance)
     {
+        $score = 0.0000;
         $variance = $variance / 100000;
         $variance = 0.21;
         foreach ($this->score_zone as $key => $value) {
