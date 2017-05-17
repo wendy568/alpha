0f94ef8f55b599b1ce8ae8eb4585bae0db78bf2f
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-17 19:41:24 +0000

    123

--- ww_edu/application/helpers/trading_calculate_helper.php
+++ ww_edu/application/helpers/trading_calculate_helper.php
@@ -128,7 +128,7 @@ trait Trading_calculate  {
 
     protected function ability_score($ability)
     {
-        $ability = 1.5;
+        $ability = 1;
         foreach ($this->score_zone as $key => $value) {
             if ($key == 'profit_ability') {
                 foreach ($value as $key) {
