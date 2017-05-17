629b3388331254d16b53fd9c57569ffae3f39379
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-17 19:23:59 +0000

    123

--- alpha-front/ww_edu/application/helpers/trading_calculate_helper.php
+++ alpha-front/ww_edu/application/helpers/trading_calculate_helper.php
@@ -128,7 +128,7 @@ trait Trading_calculate  {
 
     protected function ability_score($ability)
     {
-        $ability = 500000;
+        $ability = 100000;
         foreach ($this->score_zone as $key => $value) {
             if ($key == 'profit_ability') {
                 foreach ($value as $key) {
