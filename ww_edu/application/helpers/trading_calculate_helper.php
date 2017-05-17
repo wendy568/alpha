5e0737122d7d27341d77613d58bb714abd7824cd
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-17 18:51:49 +0000

    123

--- ww_edu/application/helpers/trading_calculate_helper.php
+++ ww_edu/application/helpers/trading_calculate_helper.php
@@ -135,7 +135,7 @@ trait Trading_calculate  {
                    if ($ability >= $key[1][0] && $ability <= $key[1][1]) {
                         if (is_string($key[2][1])) {
                             var_dump(log(10, 10));
-                            echo ($key[2][1])(round($ability / $key[2][0], 2), 10);die;
+                            echo $key[2][1](round($ability / $key[2][0], 2), 10);die;
                         }
                    }
                 }
