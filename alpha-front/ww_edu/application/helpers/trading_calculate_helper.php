72453d8849a7c88723b08e4dff31e0895da35128
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-17 16:22:06 +0000

    123

--- alpha-front/ww_edu/application/helpers/trading_calculate_helper.php
+++ alpha-front/ww_edu/application/helpers/trading_calculate_helper.php
@@ -105,7 +105,7 @@ trait Trading_calculate  {
 
     protected function ability_score($ability)
     {
-       $ability = $ability / 100000;
+       $ability = $ability / 10000;
        print_r($ability);
     }
 
