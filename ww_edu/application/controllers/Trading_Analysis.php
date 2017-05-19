632b6041401b47deb52832ee25aedce2c6f9e9fb
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-19 23:21:51 +0000

    123

--- ww_edu/application/controllers/Trading_Analysis.php
+++ ww_edu/application/controllers/Trading_Analysis.php
@@ -127,4 +127,27 @@ class Trading_Analysis extends MY_Controller
 		$response = array('archive' => array('status' => 0 ,'message' =>''));
 		encode_json($response,$data);
 	}
+
+	public function test()
+	{
+		header( 'Access-Control-Allow-Origin:*' );
+		
+		$token = $this->input->get_post('token', TRUE);
+		$start_time = $this->input->get_post('start_time', TRUE);
+		$end_time = $this->input->get_post('end_time', TRUE);
+		$finency_proc = $this->input->get_post('finency_proc', TRUE);
+		$account = $this->get_trading_account($token);
+
+		$this->load->database();
+		$this->load->helper('json');
+		// $this->load->helper('time_zone');
+		// date('Y-m-d H:i:s', time_zone::build()->sundayOfTheWeekOfEnd()->get_time_zone());die;
+		$this->load->model('TradingAnalysis');
+
+		$mt4 = $this->TradingAnalysis->export_mt4_datas($account, $finency_proc, $start_time, $end_time);
+		$this->load->library('trading_datas_calculate');
+		$data['data']['percent_ratio'] = $this->trading_datas_calculate->build($mt4, 3)->count()->property('percent_ratio', ['order_type'])->get_property();
+		$response = array('archive' => array('status' => 0 ,'message' =>''));
+		encode_json($response,$data);
+	}
 }
