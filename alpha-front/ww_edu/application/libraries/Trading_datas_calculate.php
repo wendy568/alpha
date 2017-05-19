4b6fb894c4e68c08b98c32a9650443d510d31c7a
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-19 13:18:27 +0000

    123

--- alpha-front/ww_edu/application/libraries/Trading_datas_calculate.php
+++ alpha-front/ww_edu/application/libraries/Trading_datas_calculate.php
@@ -123,6 +123,7 @@ class Trading_datas_calculate {
 		var_dump($start);
 		var_dump($nextOrLast);
 		var_dump($time);
+		var_dump(isset($time));
 		$time = isset($time) ? $time : time();
 		//date('Y-m-d', strtotime(date('Y-m-d', strtotime(date('Y-m-d', $time) . " {$start} day")) . " {$nextOrLast} day"))
 		$this->unix_time = strtotime(date('Y-m-d', strtotime(date('Y-m-d', $time) . " {$start} day")) . " {$nextOrLast} day");
