d678dbf9a9e4480a66495c64c5b61d7d3aa872d1
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-19 22:46:43 +0000

    123

--- ww_edu/application/controllers/Dashboard.php
+++ ww_edu/application/controllers/Dashboard.php
@@ -129,7 +129,7 @@ class Dashboard extends MY_Controller
 
 		$mt4 = $this->TradingAnalysis->calendar();
 		$this->load->library('trading_datas_calculate');
-		$this->trading_datas_calculate->time_filter_definition = 'Time';
+		$this->trading_datas_calculate->time_filter_definition = 'time_en';
 		$data['data']['calendar'] = $this->trading_datas_calculate->build($mt4, 3)->property('setUnixTime', [$start, $nextOrLast, $time_node])->get_week()->getWeekResult();
 		$response = array('archive' => array('status' => 0 ,'message' =>''));
 		encode_json($response,$data);
