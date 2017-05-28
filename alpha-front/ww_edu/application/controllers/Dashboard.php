5534d9727ee1c879cfef0780ef228a55ccd60bca
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-28 07:09:44 +0000

    123

--- alpha-front/ww_edu/application/controllers/Dashboard.php
+++ alpha-front/ww_edu/application/controllers/Dashboard.php
@@ -14,7 +14,7 @@ class Dashboard extends MY_Controller
 		$mt4 = $this->TradingAnalysis->export_mt4_datas();
 		$this->load->library('trading_datas_calculate');
 		print_r($this->trading_datas_calculate->build($mt4)->trading_count());die;
-		$response = array('archive' => array('status' => 0,'message' =>''));
+		$response = array('archive' => array('status' => 0 ,'message' =>''));
 	
 		encode_json($response,$data);
 	}
