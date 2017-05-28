3f2cca015bce2c4165dd89645bf7b1da9e79b6a9
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-28 16:22:02 +0000

    123

--- ww_edu/application/libraries/Trading_datas_calculate.php
+++ ww_edu/application/libraries/Trading_datas_calculate.php
@@ -121,6 +121,7 @@ class Trading_datas_calculate {
 					$day = $slice[2];
 					$start = mktime(00, 00, 00, $month, $day, $year);
 					$end = mktime(23, 59, 59, $month, $day, $year);
+					echo $start;echo $v;die;
 					if ($k == $this->time_filter_definition && ($v >= $start && $v <= $end)) {
 						$result[$val][] = $value;
 					} 
