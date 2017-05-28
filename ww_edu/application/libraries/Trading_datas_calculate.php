595227c9ce59300aec02d51f3f61407d19a110a8
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-05-28 09:15:59 +0000

    123

--- ww_edu/application/libraries/Trading_datas_calculate.php
+++ ww_edu/application/libraries/Trading_datas_calculate.php
@@ -18,14 +18,14 @@
  */
 class Trading_datas_calculate {
 
-	private $mt4 = [];
+	private $_data = [];
 	private $trading_count = 0;
 	private $this_month;
 	private $this_year;
 
 	public function build($import_datas, $month = null, $year = null)
 	{
-		$this->mt4 = $import_datas;
+		$this->_data = $import_datas;
 
 		if ($month !== null) {
 			$this->this_month = $month;
@@ -42,29 +42,53 @@ class Trading_datas_calculate {
 		return $this;
 	}
 
-	public function datas_for_date()
+	public function get_result()
 	{
-		$datas = $this->mt4;
+		return $this->_data;
+	}
+
+	public function datas_for_month()
+	{
+		$datas = $this->_data;
 		foreach ($datas as $key => $value) {
 			foreach ($value as $k => $v) {
-				if ($k == 'order_close_time' && getdate($v)['mon'] != $this->this_month && getdate($v)['year'] != $this->this_year) {
+				if ($k == 'order_close_time' && getdate($v)['year'] != $this->this_year) {
 					unset($datas[$key]);
 				}	
 			}
 		}
-		print_r($datas);
+
+		$this->_data = $datas;
+
+		return $this;
+	}
+
+	public function datas_for_month()
+	{
+		$datas = $this->_data;
+		foreach ($datas as $key => $value) {
+			foreach ($value as $k => $v) {
+				if ($k == 'order_close_time' && getdate($v)['mon'] != $this->this_month) {
+					unset($datas[$key]);
+				}	
+			}
+		}
+
+		$this->_data = $datas;
+
+		return $this;
 	}
 
 	//Count(OrderNo(OrderType=0))+Count(OrderNo(OrderType=1))
     public function trading_count()
     {
-    	$this->trading_count = count($this->mt4);
+    	$this->trading_count = count($this->_data);
     }
 
     public function profit_since_today()
     {
     	$sum = 0;
-    	array_walk_recursive($this->mt4, function ($val, $key) use (&$sum){
+    	array_walk_recursive($this->_data, function ($val, $key) use (&$sum){
     		if ($key == 'profit') {
     			$sum += $val;
     		}
