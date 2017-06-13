704ef94484040da20bf1a79ef5e35ed100ff4c0f
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-13 11:34:03 +0000

    123

--- ww_edu/application/libraries/Trading_datas_calculate.php
+++ ww_edu/application/libraries/Trading_datas_calculate.php
@@ -98,6 +98,7 @@ class Trading_datas_calculate extends Encapsulation{
     public function transaction_peroid($index)
     {
     	$time = end($this->_data)[$index];
+        print_r($time);
     	return ($time) ? time() - $time : 0;
     }
 
