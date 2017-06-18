39894f58ffb5cd3795bd6c4a0cf5fc60936f230c
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-18 18:56:54 +0000

    123

--- alpha-front/ww_edu/application/controllers/Classes.php
+++ alpha-front/ww_edu/application/controllers/Classes.php
@@ -28,7 +28,7 @@ class Classes extends MY_Controller
 		$this->load->helper('format');
 		$this->load->helper('Trading_calculate');
 		$this->load->library('classes_mission');
-		print_r(json_decode('{}'));die;
+		
 		$response = array('archive' => array('status' => 0,'message' =>'all mission complete'));
 		$original = $this->ClassesM->current_stage($uid);
 		$allProcess = $this->allProcess();
@@ -208,6 +208,7 @@ class Classes extends MY_Controller
 
 		$response = array('archive' => array('status' => 0, 'message' => ''));
 		$data['data'] = [];
+		print_r(json_decode('{}'));die;
 
 		$allProcess = $this->allProcess();
 		$history = $this->show_history($uid);
