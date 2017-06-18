e9c76c41962e0c242cfef87c4b32058331d650b8
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-18 03:56:00 +0000

    123

--- ww_edu/application/libraries/Classes_mission.php
+++ ww_edu/application/libraries/Classes_mission.php
@@ -188,7 +188,7 @@ class Classes_mission
 
 	public function make_complete($current_mission, $personal, $mission_key = null)
 	{
-		$a = '';
+		$a = -1;
 		var_dump(empty($a));
 		$mission_key = explode(',', $mission_key);
 		foreach ($mission_key as $key) {
