c62072796f73593235a33949d6db3269b23b51af
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-18 03:21:41 +0000

    123

--- alpha-front/ww_edu/application/controllers/Classes.php
+++ alpha-front/ww_edu/application/controllers/Classes.php
@@ -232,6 +232,7 @@ class Classes extends MY_Controller
 		}
 
 		if ($original['personal']['hw_id'] > $stage_id) {
+			print_r($current_mission);
 			$personal = $this->classes_mission->make_complete($current_mission, $history[$stage_id . '_'], $mission_key);
 			print_r($personal);
 		}
