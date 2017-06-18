4a3fd49a2701251ee8531e5c1ffb9b1f3819f041
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-18 22:40:12 +0000

    123

---
+++ alpha-front/ww_edu/application/models/Remarks.php
@@ -0,0 +1,26 @@
+<?php  
+
+class Remarks extends CI_Model 
+{
+	function __construct()
+    {
+        parent::__construct();
+    }
+
+	function list($uid, $start = null, $limit = null)
+	{
+		$start = (isset($start) OR $start) ? $start : 0;
+		$limit = (isset($limit) OR $limit) ? $limi : 10; 
+		$result = array();
+		$map = "SELECT *  
+				FROM remark
+				WHERE uid={$uid}
+				LIMIT {$start},{$limit}";	
+		$query = $this->db->query($map);
+        $result = $query->result_array();
+        
+        return $result;
+
+	}
+
+}
