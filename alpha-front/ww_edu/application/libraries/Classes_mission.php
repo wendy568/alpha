88ecf25d8665371ee554bf45247d5c7da0a1291c
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-18 03:55:32 +0000

    123

--- alpha-front/ww_edu/application/libraries/Classes_mission.php
+++ alpha-front/ww_edu/application/libraries/Classes_mission.php
@@ -188,7 +188,7 @@ class Classes_mission
 
 	public function make_complete($current_mission, $personal, $mission_key = null)
 	{
-		$a = 0;
+		$a = '';
 		var_dump(empty($a));
 		$mission_key = explode(',', $mission_key);
 		foreach ($mission_key as $key) {
