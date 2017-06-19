2c02a21d34bebc167d2d485cf1fb67e872f10bba
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-19 16:31:04 +0000

    123

--- alpha-front/ww_edu/application/libraries/Insert_update_api.php
+++ alpha-front/ww_edu/application/libraries/Insert_update_api.php
@@ -28,6 +28,7 @@ class Insert_update_api extends sql_operation
 		foreach ($needUpdate as $key => $value) {
 			foreach ($value as $k => $v) {
 				if ($k == 'md5') {
+					print_r($compa);
 					$needUpdate[$key] = $compa[$v];
 				}
 			}
