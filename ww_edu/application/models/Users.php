e0dc8bf5bc0df295cd26727fbb698d74d9d39b7f
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-19 17:48:02 +0000

    123

--- ww_edu/application/models/Users.php
+++ ww_edu/application/models/Users.php
@@ -112,11 +112,11 @@ class Users extends CI_Model
 		$message = '';
 		$status = 0;
 		$count = 1;
-		$update_count += $count;
 
 		array_walk($cols, function($val, $key) use (&$message, &$count){
 			if($this->db->query($val)){
-				$message = "{$key} update success ,";	
+				$message = "{$key} update success ,";
+				$update_count += $count;
 			}else{
 				$message .= "{$key} update failed, 「 {$val} 」,";
 				$status = 39;
