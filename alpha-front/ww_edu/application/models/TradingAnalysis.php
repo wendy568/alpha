3900bfffe0897fa19315110fb2d3154e6e71b929
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-19 18:27:39 +0000

    123

--- alpha-front/ww_edu/application/models/TradingAnalysis.php
+++ alpha-front/ww_edu/application/models/TradingAnalysis.php
@@ -66,7 +66,7 @@ class TradingAnalysis extends CI_Model
                 FROM calendar
                 WHERE {$where}
                 ORDER BY id DESC
-                LIMIT 3";
+                LIMIT 7";
 
         $result = $this->db->query($map)->result_array();
 
