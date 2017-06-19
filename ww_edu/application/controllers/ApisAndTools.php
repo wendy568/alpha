990f63562ffa80fe4f9c92c936ff6c1b008aa80d
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-19 16:09:14 +0000

    123

--- ww_edu/application/controllers/ApisAndTools.php
+++ ww_edu/application/controllers/ApisAndTools.php
@@ -32,7 +32,7 @@ class ApisAndTools extends MY_Controller
 	
 		$response = array('archive' => array('status' => 0,'message' =>''));
 		$calendars_before = $this->TradingAnalysis->calendarForApi();
-		$this->insert_update_api->init($calendar, $calendars_before)->comparison();
+		$this->insert_update_api->init($calendar, $calendars_before)->property('comparison');
 		die;
 		$data['data'] = $this->TradingAnalysis->calendar($calendar);
 	
