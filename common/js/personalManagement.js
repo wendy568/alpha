3346b0c4dbbab381b3b119238557436e3d1d9c9b
huaxin <huaxin@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-20 09:53:54 +0000

    进度管理bug修复

--- common/js/personalManagement.js
+++ common/js/personalManagement.js
@@ -1,4 +1,5 @@
 (function(){
+  var curStage = 0;
   // 环形进度条JS
   function getStage(stage) {
     var c1 = document.getElementById('c1');
@@ -65,180 +66,208 @@
   function getVideoList(videoList) {
       var tvList = "";
       $('.tv-list').empty();
-      $.each(videoList,function(i,data){
+      if(videoList.length){
+        $.each(videoList,function(i,data){
           tvList = $('<li id="'+ data.id +'" class="tv-small col-lg-3 col-md-4 col-sm-6 col-xs-12 no-margin m-b-30" data-toggle="modal" data-target="#tvModal">'+
-                          '<div class="bk-img">'+
-                              '<img src="'+alpha_host+'upload/'+ data.image[0] +'m_'+ data.image[1] +'" alt="" >'+
-                          '</div>'+
-                          '<img class="tv-btn img-responsive" src="./assets/img/dashboard_tv_play.png" alt="">'+
-                          '<div class="tv-des">'+
-                              '<h5 class="text-c2">'+ data.name +'</h5>'+
-                          '</div>'+
-                      '</li>');
+            '<div class="bk-img">'+
+            '<img src="'+alpha_host+'upload/'+ data.image[0] +'m_'+ data.image[1] +'" alt="" >'+
+            '</div>'+
+            '<img class="tv-btn img-responsive" src="./assets/img/dashboard_tv_play.png" alt="">'+
+            '<div class="tv-des">'+
+            '<h5 class="text-c2">'+ data.name +'</h5>'+
+            '</div>'+
+            '</li>');
           $('.tv-list').append(tvList);
-      });
-      $('.tv-list').on('click','.tv-small',function(){
+        });
+        $('.tv-list').on('click','.tv-small',function(){
           var vmid=$(this).attr('id');
           $.alpha.request_Url('post','Classes/record_process',{article_classes_id:vmid,look_up:'video'},function(data){});
           $.alpha.request_Url('post','video/videos_detail',{class_id:vmid},function(data){
-              var tvStudy="";
-              tvStudy +='<iframe id="tv" src="http://content.jwplatform.com/players/'+data.data.source+'-T351KaXB.html" height="100%" width="100%" frameborder="0" scrolling="auto" allowfullscreen></iframe>';
-              $('#tvModal .tv-detail-header').html(tvStudy);
-              var tvdesc="";
-              tvdesc +='<h3 class="text-c1 no-margin p-b-10">'+data.data.name+'</h3><P class="text-c3 no-padding">'+data.data.describe+'</P>';
-              $('#tvModal .tv-detail-body').html(tvdesc);
+            var tvStudy="";
+            tvStudy +='<iframe id="tv" src="http://content.jwplatform.com/players/'+data.data.source+'-T351KaXB.html" height="100%" width="100%" frameborder="0" scrolling="auto" allowfullscreen></iframe>';
+            $('#tvModal .tv-detail-header').html(tvStudy);
+            var tvdesc="";
+            tvdesc +='<h3 class="text-c1 no-margin p-b-10">'+data.data.name+'</h3><P class="text-c3 no-padding">'+data.data.describe+'</P>';
+            $('#tvModal .tv-detail-body').html(tvdesc);
           });
-      });
+        });
+      }
   }
+  
   // 获取articlelist
   function getArticleList(articleList) {
       var articList = "";
       $('.artic-list').empty();
-      $.each(articleList,function(i,data){
+      if(articleList.length){
+        $.each(articleList,function(i,data){
           articList = $('<li id="'+ data.id+'" class="tv-small col-lg-3 col-md-4 col-sm-6 col-xs-12 no-margin m-b-30" data-toggle="modal" data-target="#articleModal">'+
-                          '<div class="bk-img">'+
-                              '<img src="'+alpha_host+'upload/'+ data.image[0] +'m_'+ data.image[1] +'" alt="" >'+
-                          '</div>'+
-                          '<div class="tv-des">'+
-                              '<h5 class="text-c2">'+data.title+'</h5>'+
-                          '</div>'+
-                      '</li>');
+            '<div class="bk-img">'+
+            '<img src="'+alpha_host+'upload/'+ data.image[0] +'m_'+ data.image[1] +'" alt="" >'+
+            '</div>'+
+            '<div class="tv-des">'+
+            '<h5 class="text-c2">'+data.title+'</h5>'+
+            '</div>'+
+            '</li>');
           $('.artic-list').append(articList);
-      });
-      $('.artic-list').on('click','.tv-small',function(){
+        });
+        $('.artic-list').on('click','.tv-small',function(){
           var aid=$(this).attr('id');
           $.alpha.request_Url('post','Classes/article_detail',{article_id:aid},function(data){
-              var artdesc="";
-              artdesc +=['<div class="text-c1 tv-close close" data-dismiss="modal" style="top:0;opacity: 1;">X</div>',
-                              '<h3 class="text-c1 no-margin p-b-10">',
-                                  '<i class="status-icon yellow m-r-10 m-b-5"></i>',
-                                  data.data.title+'</h3>',
-                              '<p>'+data.data.update_time+'</p>',
-                              '<P class="text-c3 no-padding">'+data.data.content+'</P>'].join('');
-              $('#articleModal .tv-detail-body').html(artdesc);
+            var artdesc="";
+            artdesc +=['<div class="text-c1 tv-close close" data-dismiss="modal" style="top:0;opacity: 1;">X</div>',
+              '<h3 class="text-c1 no-margin p-b-10">',
+              '<i class="status-icon yellow m-r-10 m-b-5"></i>',
+              data.data.title+'</h3>',
+              '<p>'+data.data.update_time+'</p>',
+              '<P class="text-c3 no-padding">'+data.data.content+'</P>'].join('');
+            $('#articleModal .tv-detail-body').html(artdesc);
           });
-      });
+        });
+      }
   }
-  //  获取其他课程内容
-  function getStudyList(stage,stageDtail){
+  
+  //  获取课程内容
+  function getStudyList(stage,stageDtail,complete){
         var content = '';
         for(var i in stageDtail){
             if(typeof(stageDtail[i]) == 'string' || typeof(stageDtail[i]) == 'number'){
               stageDtail[i] = stageDtail[i] + '';
-              content = stageDtail[i].indexOf('/') > -1 ? stageDtail[i].split('/')[0] : stageDtail[i];
+              content = curStage == stage ? stageDtail[i].split('/')[0] : (stage>curStage ? 0 : stageDtail[i]);
             }
             var index = i;
+            var isComplete = 'fa fa-circle-o text-c3';
+            if(stage<curStage){
+              isComplete = 'complete';
+            }else if(stage>curStage){
+              isComplete = 'fa fa-circle-o text-c3';
+            }else{
+              isComplete = (complete && complete[i]) ? 'complete' : 'fa fa-circle-o text-c3';
+            }
+            var setComplete = function (name) {
+              $('#stage'+ stage + name).html(content);
+              $('#stage'+ stage + name).parents('li.panel').find('i').attr('class',isComplete);
+            };
             switch (i.toLocaleLowerCase()){
-                case 'video learning':getVideoList(stageDtail[index])
+                case 'video learning':getVideoList(stageDtail[index]);
+                                      $('#stage'+ stage + ' .tv-list').parents('li.panel').find('i').attr('class',isComplete);
                     break;
-                case 'article learning':getArticleList(stageDtail[index])
+                case 'article learning':getArticleList(stageDtail[index]);
+                                      $('#stage'+ stage + ' .artic-list').parents('li.panel').find('i').attr('class',isComplete);
                     break;
-                case 'place your order':$('#stage'+ stage + ' .placeYourOrder').html(content)
+                case 'place your order':setComplete(' .placeYourOrder');
                     break;
-                case '4 style trade':$('#stage'+ stage + ' .4StyleTrade').html(content)
+                case '4 style trade':setComplete(' .4StyleTrade');
                     break;
-                case 'take profits/stop loss':$('#stage'+ stage + ' .stopLoss').html(content)
+                case 'take profits/stop loss':setComplete(' .stopLoss');
                    break;
-                case 'make transactions':$('#stage'+ stage + ' .makeTransactions').html(content)
+                case 'make transactions':setComplete(' .makeTransactions');
                     break;
-                case 'trade all kinds products':$('#stage'+ stage + ' .tradeAllKindsProducts').html(content)
+                case 'trade all kinds products':setComplete(' .tradeAllKindsProducts');
                     break;
-                case 'trading record':$('#stage'+ stage + ' .tradingRecord').html(content)
+                case 'trading record':setComplete(' .tradingRecord');
                     break;
-                case 'learning report':$('#stage'+ stage + ' .learningReport').html(content)
+                case 'learning report':setComplete(' .learningReport');
                     break;
-                case 'make transaction 1':$('#stage'+ stage + ' .makeTransactionOn').html(content)
+                case 'make transaction 1':setComplete(' .makeTransactionOn');
                     break;
-                case 'make transaction 2':$('#stage'+ stage + ' .makeTransactionTwo').html(content)
+                case 'make transaction 2':setComplete(' .makeTransactionTwo');
                     break;
-                case 'task 1 - 2 different markets':$('#stage'+ stage + ' .2DifferentMarkets').html(content)
+                case 'task 1 - 2 different markets':setComplete(' .2DifferentMarkets');
                     break;
-                case 'task 2 - 10 different products':$('#stage'+ stage + ' .10DifferentProducts').html(content)
+                case 'task 2 - 10 different products':setComplete(' .10DifferentProducts');
                     break;
-                case '5 tradable products':$('#stage'+ stage + ' .5TradableProducts').html(content)
+                case '5 tradable products':setComplete(' .5TradableProducts');
                     break;
-                case 'produce a module':$('#stage'+ stage + ' .produceAModule').html(content)
+                case 'produce a module':setComplete(' .produceAModule');
                     break;
-                case 'risk management level':$('#stage'+ stage + ' .riskManagementLevel').html(content)
+                case 'risk management level':setComplete(' .riskManagementLevel');
                     break;
-                case 'trading score':$('#stage'+ stage + ' .tradingScore').html(content)
+                case 'trading score':setComplete(' .tradingScore');
                     break;
-                case 'profitable period':$('#stage'+ stage + ' .profitablePeriod').html(content)
+                case 'profitable period':setComplete(' .profitablePeriod');
                     break;
             }
         }
     }
 
-	$.alpha.request_Url('post','Classes/current_stage',{},function(data){
-		// 阶段展示
-		var stage=data.data.current_stage;
-		getStage(stage);
-		if(stage > 8){
-      $('#levelList').scrollLeft(stage*147);
-    }
-		// 完成情况
-		var complete=data.data.complete;
-		$('.description span').html(complete*100 + '%');
-		$('.stage-left .progress').html('<div class="progress-bar progress-bar-success animate-progress-bar" data-percentage="'+ complete*100 +'%" style="width:'+ complete*100 +'%"></div>');
-
-		// title
-		$('.pro-intr h4').html(data.data.title);
-
-        // 阶段描述点击获取全文
-        var desc=data.data.describe.substr(0,80);
-        var i=0;
-        $('.more').html('Continue Reading...');
-        $('.more').click(function(){
+  function getCurStage() {
+    $.alpha.request_Url('post','Classes/current_stage',{},function(data){
+      // 阶段展示
+      var stage=curStage=data.data.current_stage;
+      getStage(stage);
+      if(stage > 8){
+        $('#levelList').scrollLeft(stage*147);
+      }
+      // 完成情况
+      var complete=data.data.complete;
+      $('.description span').html(complete*100 + '%');
+      $('.stage-left .progress').html('<div class="progress-bar progress-bar-success animate-progress-bar" data-percentage="'+ complete*100 +'%" style="width:'+ complete*100 +'%"></div>');
+    
+      // title
+      $('.pro-intr h4').html(data.data.title);
+    
+      // 阶段描述点击获取全文
+      var desc=data.data.describe.substr(0,80);
+      var i=0;
+      $('.more').html('Continue Reading...');
+      $('.more').click(function(){
+        if(i == 0){
+          desc=data.data.describe;
+          $(this).html('Pack Up The Full...');
+          i = i +1;
+        }else{
+          desc=data.data.describe.substr(0,80);
+          $(this).html('Continue Reading...');
+          i = 0;
+        }
+        $('.pro-intr p').html(desc);
+      });
+      $('.pro-intr p').html(desc);
+    
+      // 定位 stage learning
+      $('.roll-list li').removeClass('active').eq(stage-1).addClass('active');
+      $('.tab-pane').removeClass('active').eq(stage-1).addClass('active');
+      
+      // 获取课程内容
+      var stageDtail=data.data.detail;
+      getStudyList(stage,stageDtail,data.data.is_complete);
+    });
+  }
+  
+  getCurStage();
+  
+  $('.module').click(function(){
+      var stage_id = parseInt($(this).index()) + 1;
+      if(stage_id!=curStage){
+        $.alpha.request_Url('post','Classes/showStageDetail',{stage_id:stage_id},function(data){
+    
+          // title
+          $('.pro-intr h4').html(data.data.title);
+    
+          // 阶段描述点击获取全文
+          var desc=data.data.describe.substr(0,80);
+          var i=0;
+          $('.more').html('Continue Reading...');
+          $('.more').click(function(){
             if(i == 0){
-                desc=data.data.describe;
-                $(this).html('Pack Up The Full...');
-                i = i +1;
+              desc=data.data.describe;
+              $(this).html('Pack Up The Full...');
+              i = i +1;
             }else{
-                desc=data.data.describe.substr(0,80);
-                $(this).html('Continue Reading...');
-                i = 0;
+              desc=data.data.describe.substr(0,80);
+              $(this).html('Continue Reading...');
+              i = 0;
             }
             $('.pro-intr p').html(desc);
+          });
+          $('.pro-intr p').html(desc);
+    
+          var stageDtail=data.data.detail;
+          getStudyList(stage_id,stageDtail);
         });
-        $('.pro-intr p').html(desc);
-
-		// 定位 stage learning
-		$('.roll-list li').removeClass('active');
-		$('.tab-pane').removeClass('active');
-		$('.roll-list li').eq(stage-1).addClass('active');
-		$('.tab-pane').eq(stage-1).addClass('active');
-
-		// 获取课程内容
-		var stageDtail=data.data.detail;
-        getStudyList(stage,stageDtail);
-	});
-
-    $('.module').click(function(){
-        var stage_id = parseInt($(this).index()) + 1;
-        $.alpha.request_Url('post','Classes/showStageDetail',{stage_id:stage_id},function(data){
-            // title
-            $('.pro-intr h4').html(data.data.title);
-             // 阶段描述点击获取全文
-            var desc=data.data.describe.substr(0,80);
-            var i=0;
-            $('.more').html('Continue Reading...');
-            $('.more').click(function(){
-                if(i == 0){
-                    desc=data.data.describe;
-                    $(this).html('Pack Up The Full...');
-                    i = i +1;
-                }else{
-                    desc=data.data.describe.substr(0,80);
-                    $(this).html('Continue Reading...');
-                    i = 0;
-                }
-                $('.pro-intr p').html(desc);
-            });
-            $('.pro-intr p').html(desc);
-
-            var stageDtail=data.data.detail;
-            getStudyList(stage_id,stageDtail);
-        });
-    });
-   
+      }else{
+        getCurStage();
+      }
+  });
+  
 })();
