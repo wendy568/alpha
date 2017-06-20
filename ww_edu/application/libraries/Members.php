d5d7ca9c98f054301584bf960518d1775ecece0f
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-20 10:00:52 +0000

    123

---
+++ ww_edu/application/libraries/Members.php
@@ -0,0 +1,37 @@
+<?php  
+
+class Members extends sql_operation
+{
+	use struct;
+
+	public function init($array)
+	{
+		$this->_data = $array;
+		$this->_data['table'] = ['member', 'user_info'];
+		$this->_data['update_time'] = date('Y-m-d H:i:s', time());
+
+		return $this;
+	}
+
+	public function update($file = null)
+	{
+		$instance = & get_instance();
+		$instance->load->helper('databases_filter');
+		$instance->load->helper('set_source');
+		$image = null;
+
+		if(!empty($this->width) && !empty($this->height)) $image = get_image($this->width, $this->height, "{$this->file_path}");
+		if($image) $this->_data[$file] = addslashes(json_encode(array("{$this->file_path}/".json_decode($image, TRUE)[0],json_decode($image, TRUE)[1])));
+
+		$dfdb = databases_filter::build();
+		$cols = $this->table;
+
+		$dfdb->set_query($cols, $this->_data)
+		     ->filter_blank($cols)
+			 ->update_complete($cols, array('member' => array('id'=>$this->id), 'user_info' => $this->mem_id));
+
+		return $cols;
+		
+	}
+
+}
