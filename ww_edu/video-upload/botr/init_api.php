bb08c70c524472b358a5fb1be80c4c71e4bb5bf8
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-20 19:40:27 +0000

    123

--- ww_edu/video-upload/botr/init_api.php
+++ ww_edu/video-upload/botr/init_api.php
@@ -5,5 +5,5 @@
 
 require_once('api.php');
 // Replace xxxx and yyyy with your API key/secret (can be found in the CMS).
-$botr_api = new BotrAPI('xxxx', 'yyyyyyyy');
+$botr_api = new BotrAPI('c6PABMlh', 'CLJeFvRzMIyA4bsB4SV5eZlH');
 ?>
