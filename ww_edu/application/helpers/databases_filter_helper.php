0b4868ba694aa599fab958c218d168dcd2621f55
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-21 02:52:39 +0000

    123

--- ww_edu/application/helpers/databases_filter_helper.php
+++ ww_edu/application/helpers/databases_filter_helper.php
@@ -55,6 +55,7 @@ class databases_filter{
 
     function update_complete(&$data, $where)
     {
+        var_dump($where);
     	$result = '';
     	foreach($data as $key=>&$val) {
             foreach ($val as $k =>&$v) {
