222f692cbe86ff9d26cf6afd3be3ae289441c13d
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-23 07:40:35 +0000

    123

--- ww_edu/application/controllers/Admin.php
+++ ww_edu/application/controllers/Admin.php
@@ -26,10 +26,11 @@ class Admin extends MY_Controller
 		$data = array();
 		$username = $this->input->get_post('username', TRUE);
 		$password = $this->input->get_post('password', TRUE);
+		print_r($_POST);
 		$this->load->database();
 		$this->load->helper('json');
 		$this->load->model('admins');
-		$response = array('archive' => array('status' => 0,'message' =>''));
+		$response = array('archive' => array('status' => 0 ,'message' =>''));
 		$this->admins->login($username, $password, $response, $data);
 		
 		encode_json($response,$data);
