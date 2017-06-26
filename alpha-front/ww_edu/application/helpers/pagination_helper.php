2ec4c6204114be896a05d660c7cb8bd7167ede1a
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-26 08:37:05 +0000

    123

--- alpha-front/ww_edu/application/helpers/pagination_helper.php
+++ alpha-front/ww_edu/application/helpers/pagination_helper.php
@@ -22,12 +22,13 @@ trait pagination
 
         if ($total_pages == 0) return false;
         if ($this->pages > $total_pages) $this->pages = $total_pages;
-        
+        if ($this->pages < 0) $this->pages = 1;
+
         for ($this->pages; $this->pages <= $total_pages; $this->pages++) {
             $offset = ($this->pages - 1) * $this->page_nums_per;
             $result["_{$this->pages}"] = array_slice($this->_array,$offset, $this->page_nums_per);
         }
-        
+
         $result['total_pages'] = $total_pages;
         $result['total_nums'] = $this->total_nums;
         print_r($result);
