e9b007605016921af10c4774b4ad0f1f217c9ca8
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-27 06:55:30 +0000

    123

--- ww_edu/application/helpers/pagination_helper.php
+++ ww_edu/application/helpers/pagination_helper.php
@@ -22,7 +22,7 @@ trait pagination
         if ($pages * $page_nums_per > $limit) {
             $multiplying = ceil(($pages * $page_nums_per) / $limit);
             $start += $multiplying * $limit + 1;
-            $this->start = $multiplying * $limit;
+            $this->start = $multiplying * $page_nums_per;
 
             return true;
         } else {
