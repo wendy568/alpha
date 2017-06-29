a01d86d5f07aa0f5cf6b838d1383200504a55b27
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-29 06:47:06 +0000

    123

--- alpha-front/ww_edu/application/controllers/Classes.php
+++ alpha-front/ww_edu/application/controllers/Classes.php
@@ -154,6 +154,7 @@ class Classes extends MY_Controller
 		$mission = $this->classes_mission->jsonDecode($original['mission']['homework']);
 		$personal = $this->classes_mission->jsonDecode($original['personal']['homework']);
 		foreach ($history_homework as $key => $value) {
+			print_r($value);
 			$data['list'][$key][] = $this->classes_mission->init($mission, $value, $allProcess)->generating()->get_mission_complete()->property('distributing')->getOneComplete();
 		}
 		$data['list']['_' . $original['personal']['hw_id']] = $this->classes_mission->init($mission, $personal, $allProcess)->generating()->get_mission_complete()->property('distributing')->getOneComplete();
