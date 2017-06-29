91da1b95325753a65831326623f762fb5e920661
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-29 07:44:15 +0000

    123

--- alpha-front/ww_edu/application/controllers/Classes.php
+++ alpha-front/ww_edu/application/controllers/Classes.php
@@ -161,9 +161,9 @@ class Classes extends MY_Controller
 			$history_mission = $this->get_mission(substr($key, -1));
 			$list[$key] = $this->classes_mission->init(json_decode($history_mission['homework'], true), $value)->learnOneComplete()->get_mission_complete()->property('distributing')->getOneComplete();
 		}
-		print_r($list);
+
 		foreach ($allProcess as $value) {
-			print_r($value);
+			print_r($list['_' . $value['id']]);
 			if (!empty($list['_' . $value['id']])) unset($allProcess[$value['id']]);
 		}
 		print_r($allProcess);die;
