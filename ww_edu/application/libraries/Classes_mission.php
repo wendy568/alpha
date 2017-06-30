3ef653bf0eeed73c7c4c0a38022b862b86d4f0af
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-30 10:22:32 +0000

    123

--- ww_edu/application/libraries/Classes_mission.php
+++ ww_edu/application/libraries/Classes_mission.php
@@ -190,6 +190,7 @@ class Classes_mission
 	{
 		$mission_key = explode(',', $mission_key);
 		foreach ($mission_key as $key) {
+			var_dump($key);
 			if (isset($mission_key) && $mission_key) $personal[$key] = $current_mission[$key];
 
 		}
