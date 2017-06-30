6dd9d510c999aa3ea7129a66cde01098fd808f92
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-06-30 09:23:06 +0000

    123

--- ww_edu/application/libraries/Classes_mission.php
+++ ww_edu/application/libraries/Classes_mission.php
@@ -193,7 +193,7 @@ class Classes_mission
 		print_r(explode(',', $mission_key));
 		$mission_key = explode(',', $mission_key);
 		var_dump($mission_key);
-		$a = '';
+		$a = null;
 		var_dump(isset($a));
 		foreach ($mission_key as $key) {
 			var_dump(is_null($key));
