7efba14518afce1c253f8f33fccb4e15faef4e36
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-07-06 02:42:06 +0000

    123

--- alpha-front/ww_edu/video-upload/botr/init_api.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Include and init BotR API.
- */
-
-require_once('api.php');
-// Replace xxxx and yyyy with your API key/secret (can be found in the CMS).
-$botr_api = new BotrAPI('c6PABMlh', 'CLJeFvRzMIyA4bsB4SV5eZlH');
-?>
