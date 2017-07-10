1733611b0aed3a9104097251c77b9e0b14515c26
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-07-10 09:07:27 +0000

    123

--- alpha-front/ww_edu/application/controllers/Personal.php
+++ alpha-front/ww_edu/application/controllers/Personal.php
@@ -377,6 +377,7 @@ class Personal extends MY_Controller
 		    while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
 		    	static $i=0;
 		    	array_walk($data, function ($val, $key) use ($mt4_format, &$datas, $i) {
+		    		print_r($key);
 		    		if(isset($val)) $datas[$i][$mt4_format[$key]] = $val;	
 		    	});
 		    	// $datas[] = array_combine($mt4_format, $data);
