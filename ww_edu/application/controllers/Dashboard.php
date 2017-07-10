49c6d32d6926b907b6497b571610bf45a87623ff
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-07-10 09:17:22 +0000

    123

--- ww_edu/application/controllers/Dashboard.php
+++ ww_edu/application/controllers/Dashboard.php
@@ -86,7 +86,7 @@ class Dashboard extends MY_Controller
 		// $this->load->helper('time_zone');
 		// date('Y-m-d H:i:s', time_zone::build()->sundayOfTheWeekOfEnd()->get_time_zone());die;
 		$this->load->model('TradingAnalysis');
-		print_r($openCapital);
+
 		$mt4 = $this->TradingAnalysis->export_mt4_datas($account);		
 		$this->load->helper('encapsulation');
 		$this->load->library('trading_datas_calculate');
