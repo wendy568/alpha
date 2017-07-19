2d24347dc9eeaef61caf156c07a7e461248dab5e
huaxin <huaxin@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-07-19 06:29:57 +0000

    忘记密码

--- common/js/login.js
+++ common/js/login.js
@@ -4,6 +4,7 @@
 $(function () {
     var emailReg = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     var pwdReg = /^[A-Za-z0-9]{6,12}$/;
+    var accountReg = /^[A-Za-z0-9]{4,20}$/;
     
     // Login
     $('#btn-login').click(function (e) {
@@ -60,25 +61,23 @@ $(function () {
         }
     });
     
-
-
     // Register
     $('#btn-register').click(function (e) {
         var event = e || window.event;
         event.stopPropagation();
         sessionStorage.setItem('alpha_token', '');
-        var account = $('#tab_register [name="account"]').val().trim(),
-            pwd = $('#tab_register [name="pwd"]').val().trim(),
-            inviteCode = $('#tab_register [name="inviteCode"]').val().trim();
-            firstName = $('#tab_register [name="firstName"]').val().trim(),
-            lastName = $('#tab_register [name="lastName"]').val().trim(),
-            birth = $('#tab_register [name="birth"]').val().trim(),
-            sex = $('#tab_register [name="sex"]').val().trim(),
-            email = $('#tab_register [name="email"]').val().trim(),
-            emailCode = $('#tab_register [name="emailCode"]').val().trim(),
-            mt4Account = $('#tab_register [name="mt4Account"]').val().trim(),
-            mt4Group = $('#tab_register [name="mt4Group"]').val().trim(),
-            mt4Server = $('#tab_register [name="mt4Server"]').val().trim();
+        var account = $('#tab_register [name="account"]').val(),
+            pwd = $('#tab_register [name="pwd"]').val(),
+            inviteCode = $('#tab_register [name="inviteCode"]').val();
+            firstName = $('#tab_register [name="firstName"]').val(),
+            lastName = $('#tab_register [name="lastName"]').val(),
+            birth = $('#tab_register [name="birth"]').val(),
+            sex = $('#tab_register [name="sex"]').val(),
+            email = $('#tab_register [name="email"]').val(),
+            emailCode = $('#tab_register [name="emailCode"]').val(),
+            mt4Account = $('#tab_register [name="mt4Account"]').val(),
+            mt4Group = $('#tab_register [name="mt4Group"]').val(),
+            mt4Server = $('#tab_register [name="mt4Server"]').val();
         
 
         var data = {
@@ -116,16 +115,7 @@ $(function () {
         }
         
     });
-    $('#tab_register [name="account"]').change(function (e) {
-        var $this = $(this);
-        if(!$this.val().trim()){
-            setTimeout(function(){
-                $.alpha.props($this,'right','Can\'t Empty!');
-            },400);
-        }else{
-            $.alpha.props($this,'none');
-        }
-    });
+    
     $('#tab_register [name="email"]').change(function () {
         if (emailReg.test($(this).val())) {
             $.alpha.props($(this), 'none');
@@ -195,13 +185,13 @@ $(function () {
     });
     
     
-
     // 忘记密码
+    var isCode = false;
     $('#forgot_next_btn').click(function (e) {
         var account = $('#tab_forgot_password input[name="account"]').val().trim();
         var email = $('#tab_forgot_password input[name="email"]').val().trim();
         var inviteCode = $('#tab_forgot_password input[name="code"]').val().trim();
-        var isNext = account && email && inviteCode && emailReg.test(email);
+        var isNext = accountReg.test(account) && emailReg.test(email) && isCode;
         if (true){
             $('#forgot_password1').hide();
             $('#forgot_password2').show();
@@ -224,6 +214,8 @@ $(function () {
     $('#forgot_submit_btn').click(function (e) {
         var password = $('#tab_forgot_password input[name="newPassword"]').val();
         var password2 = $('#tab_forgot_password input[name="new_pwdAgain"]').val();
+        var account = $('#tab_forgot_password input[name="account"]').val().trim();
+        var email = $('#tab_forgot_password input[name="email"]').val().trim();
         if (password && password2 == password){
         
         }
@@ -238,53 +230,65 @@ $(function () {
     
     $('#tab_forgot_password input[name="account"]').on('change',function (e) {
         var $this = $(this);
-        if (!$this.val().trim()){
+        $this.parent().find('.check').remove();
+        if (!accountReg.test($this.val().trim())){
             setTimeout(function () {
-                $.alpha.props($this, 'right', 'Can\'t Empty!');
+                $.alpha.props($this, 'right', 'Only 4 to 20 words and numbers!');
             },400)
         }else{
             $.alpha.props($this, 'none');
+            $this.parent().append($('<span class="fa fa-check check text-success"></span>'));
         }
     })
     $('#tab_forgot_password input[name="email"]').on('change',function (e) {
         var $this = $(this);
+        $this.parent().find('.check').remove();
         if (!$this.val().trim() || !emailReg.test($this.val().trim())){
             setTimeout(function () {
                 $.alpha.props($this, 'right', 'Invalid email!');
             },400)
         }else{
             $.alpha.props($this, 'none');
+            $this.parent().append($('<span class="fa fa-check check text-success"></span>'));
         }
     })
     $('#tab_forgot_password input[name="code"]').on('change',function (e) {
         var $this = $(this);
+        $this.parent().find('.check').remove();
         if (!$this.val().trim()){
             setTimeout(function () {
-                $.alpha.props($this, 'right', 'Invalid code!');
+                // to request
+                $this.parent().append($('<span class="fa fa-close check text-danger"></span>'));
+                isCode = false;
             },400)
         }else{
             $.alpha.props($this, 'none');
+            $this.parent().append($('<span class="fa fa-check check text-success"></span>'));
+            isCode = true;
         }
     })
     $('#tab_forgot_password input[name="newPassword"]').on('change',function (e) {
         var $this = $(this);
+        $this.parent().find('.check').remove();
         if (!pwdReg.test($this.val())){
             setTimeout(function () {
                 $.alpha.props($this, 'right', 'Invalid password!');
-                
             },400)
         }else{
             $.alpha.props($this, 'none');
+            $this.parent().append($('<span class="fa fa-check check text-success"></span>'));
         }
     })
     $('#tab_forgot_password input[name="new_pwdAgain"]').on('change',function (e) {
         var $this = $(this);
+        $this.parent().find('.check').remove();
         if ($this.val() !== $('#tab_forgot_password input[name="newPassword"]').val()){
             setTimeout(function () {
-                $.alpha.props($this, 'right', 'Different width password!');
+                $.alpha.props($this, 'right', 'Must Type the Same Password!');
             },400)
         }else{
             $.alpha.props($this, 'none');   
+            $this.parent().append($('<span class="fa fa-check check text-success"></span>'));
         }
     })
 });
