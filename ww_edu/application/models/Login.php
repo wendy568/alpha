e328bbb3f88cade93348a7c34aaf121d9080979c
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-07-25 11:03:22 +0000

    123

--- ww_edu/application/models/Login.php
+++ ww_edu/application/models/Login.php
@@ -8,16 +8,24 @@ class Login extends CI_Model
     }
 
 	function login($account, $password, &$response, &$data)
-	{
-
+	// {
+	// 	$pos = strpos($account, '@');
+	// 	if ($pos === false) {
 			$result = array();
 			$map = 'SELECT id,password 
 					FROM member 
-					WHERE email="'.$account.'"';	
+					WHERE username="'.$account.'"';	
 			$query = $this->db->query($map);
 	        $result = $query->row_array();
+		// } else {
+		// 	$result = array();
+		// 	$map = 'SELECT id,password 
+		// 			FROM member 
+		// 			WHERE email="'.$account.'"';	
+		// 	$query = $this->db->query($map);
+	 //        $result = $query->row_array();
+		// }
 		print_r($result);die;
-		
         if(isset($result))
         {
         	$hash = password_hash($result['password'], PASSWORD_BCRYPT);
