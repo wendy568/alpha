790f7e662bcd88f15338f5a1ae87aa949b0a8d5d
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-08-14 06:42:35 +0000

    123

---
+++ alpha-index/js/TalentDetail.js
@@ -0,0 +1,42 @@
+(function(){
+    // 行程安排
+	$('.outside-circle img').click(function(){
+        var imgIndex = $(this).index();
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+        $('.inside-circle').eq(imgIndex).removeClass('hide').siblings().addClass('hide');
+    });
+
+    // 往期学员
+    $('.plan-time-list').click(function(){
+    	var dayIndex = $(this).index();
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.plan-day').eq(dayIndex - 1).removeClass('hide').siblings().addClass("hide");
+    });
+
+    // Modal-Table
+    $('.custom').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+    $('.camp').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+   // default total
+    $('.total').html( "€ 99.00 * 1 = € 99.00");
+
+    // addPeople
+    $('#personalModal .add-btn').click(function(){
+        addPeople('#personalModal');
+    });
+
+    $('#enterpriseModal .add-btn').click(function(){
+        addPeople('#enterpriseModal');
+    });
+
+    // deletePeople
+    deletePeople('#personalModal');
+    deletePeople('#enterpriseModal');
+
+    new YMDselect('year1','month1','day1');
+})();
