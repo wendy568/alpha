6d23bbc1e8b664030156120c3bc36ac1b11a51a9
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-08-15 02:44:54 +0000

    123

--- v2.0/routes/api.php
+++ v2.0/routes/api.php
@@ -12,8 +12,8 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-echo 123;
-Route::get('/Pay', 'Paycontroller@index');
+
+Route::post('/Pay', 'Paycontroller@index');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
