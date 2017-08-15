ca9172cb22f1981edb807a644d76b192f8cf05f6
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-08-15 02:43:24 +0000

    123

--- alpha-front/v2.0/routes/api.php
+++ alpha-front/v2.0/routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+echo 123;
 Route::get('/Pay', 'Paycontroller@index');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
