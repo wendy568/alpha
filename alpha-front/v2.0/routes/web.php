e47bf60054fe5d108bffda6562bc43236181bedf
chenqi <chenqi@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-08-15 03:00:09 +0000

    123

--- alpha-front/v2.0/routes/web.php
+++ alpha-front/v2.0/routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::resource('pay', 'PayController@index');
+Route::resource('pay', 'PayController');
 
 Route::get('/', function () {
     return view('welcome');
