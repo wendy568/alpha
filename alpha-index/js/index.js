a5c1759fa4e2b265ec7b0ffd489912a755158d5c
huaxin <huaxin@f9af3ea2-0566-47dd-bf3e-badec6d4e64d>
2017-08-25 09:16:00 +0000

    视频bug修复

--- alpha-index/js/index.js
+++ alpha-index/js/index.js
@@ -2,10 +2,8 @@
    // 视频播放窗口
     request_Url('post','video/videos_detail',{'class_id':9},function(data){
         // 视频播放窗口
-        var html="";
-        html+= '<iframe id="tv" src="http://content.jwplatform.com/players/'+data.data.source+'-LyD0AYgf.html" width="536" height="296" frameborder="0" allowfullscreen name="tv"></iframe>';
-        $('.index-video').html(html);
-       
+        var $html = $('<iframe id="tv" src="http://content.jwplatform.com/players/'+data.data.source+'-LyD0AYgf.html" width="536" height="296" frameborder="0" allowfullscreen name="tv"></iframe>');
+        $('.index-video').html('').append($html);
     });
     // learning  tabs
     $('.list-bottom li').hover(function(){
